ea9cc85ffe37c72e32488a5ac0871d34eb35c610
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-08 14:42:29 -0700

    Factor out "read_sha1_file" into mapping/inflating/unmapping.
    This allows us to also actually check the sha1 hash using these
    routines. Needed for the "fsck" thing.

--- read-cache.c
+++ read-cache.c
@@ -1,10 +1,12 @@
 DECL|function|error
 DECL|function|get_sha1_hex
 DECL|function|hexval
+DECL|function|map_sha1_file
 DECL|function|read_cache
 DECL|function|read_sha1_file
 DECL|function|sha1_file_name
 DECL|function|sha1_to_hex
+DECL|function|unpack_sha1_file
 DECL|function|usage
 DECL|function|verify_hdr
 DECL|function|write_sha1_buffer
