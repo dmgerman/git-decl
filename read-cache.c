f25eace09f33d02ddfb23fb0b70f94c964b30190
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-09 12:09:27 -0700

    Make "write_cache()" and friends available as generic routines.
    This is needed for the change to make "read-tree" just read into the
    cache (and then you do a "checkout-cache" to update your current dir
    contents).

--- read-cache.c
+++ read-cache.c
@@ -1,3 +1,4 @@
+DECL|function|add_cache_entry
 DECL|function|cache_match_stat
 DECL|function|cache_name_compare
 DECL|function|cache_name_pos
@@ -8,11 +9,13 @@ DECL|function|hexval
 DECL|function|map_sha1_file
 DECL|function|read_cache
 DECL|function|read_sha1_file
+DECL|function|remove_file_from_cache
 DECL|function|sha1_file_name
 DECL|function|sha1_to_hex
 DECL|function|unpack_sha1_file
 DECL|function|usage
 DECL|function|verify_hdr
+DECL|function|write_cache
 DECL|function|write_sha1_buffer
 DECL|function|write_sha1_file
 DECL|variable|active_alloc
--- update-cache.c
+++ update-cache.c
@@ -1,7 +1,4 @@
-DECL|function|add_cache_entry
 DECL|function|add_file_to_cache
 DECL|function|index_fd
 DECL|function|main
-DECL|function|remove_file_from_cache
 DECL|function|verify_path
-DECL|function|write_cache
