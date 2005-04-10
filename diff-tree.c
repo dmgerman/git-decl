6b7711503134891d9755c5408563dd32a2b652b1
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-10 14:03:58 -0700

    Add "-R" flag to "diff-tree", so that it will recursively traverse a tree of trees as it diffs them.
    This makes diff-tree usable again in the new world order.

--- diff-tree.c
+++ diff-tree.c
@@ -1,6 +1,8 @@
 DECL|function|compare_tree_entry
 DECL|function|diff_tree
+DECL|function|diff_tree_sha1
 DECL|function|extract
 DECL|function|main
 DECL|function|show_file
 DECL|function|update_tree_entry
+DECL|variable|recursive
