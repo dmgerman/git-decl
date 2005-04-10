a872f63dc1cf3877bf57de6682e487c66c7bee91
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-10 11:32:54 -0700

    Make "update-cache" a bit friendlier to use (and harder to mis-use).
    It now requires the "--add" flag before you add any new files, and
    a "--remove" file if you want to mark files for removal. And giving
    it the "--refresh" flag makes it just update all the files that it
    already knows about.

--- update-cache.c
+++ update-cache.c
@@ -1,4 +1,8 @@
 DECL|function|add_file_to_cache
 DECL|function|index_fd
 DECL|function|main
+DECL|function|refresh_cache
+DECL|function|refresh_entry
 DECL|function|verify_path
+DECL|variable|allow_add
+DECL|variable|allow_remove
