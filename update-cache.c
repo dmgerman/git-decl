c12290e074488536f3fc522d4978f9c8de9f3473
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-11 09:39:21 -0700

    Make "update-cache --refresh" do what it really should do: just refresh the "stat" information.
    We need this after having done a "read-tree", for example, when the
    stat information does not match the checked-out tree, and we want to
    start getting efficient cache matching against the parts of the tree
    that are already up-to-date.

--- update-cache.c
+++ update-cache.c
@@ -1,6 +1,9 @@
 DECL|function|add_file_to_cache
+DECL|function|compare_data
+DECL|function|fill_stat_cache_info
 DECL|function|index_fd
 DECL|function|main
+DECL|function|match_data
 DECL|function|refresh_cache
 DECL|function|refresh_entry
 DECL|function|verify_path
