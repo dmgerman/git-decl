4daace1a5f3d1e7bb802d16e67ed791a15b49daa
Petr Baudis <pasky@ucw.cz>
2005-04-13 02:28:48 -0700

    [PATCH] Consolidate the error handling
    Now there is error() for "library" errors and die() for fatal "application"
    errors. usage() is now used strictly only for usage errors.
    
    Signed-off-by: Petr Baudis <pasky@ucw.cz>

--- read-cache.c
+++ read-cache.c
@@ -3,6 +3,7 @@ DECL|function|cache_match_stat
 DECL|function|cache_name_compare
 DECL|function|cache_name_pos
 DECL|function|check_sha1_signature
+DECL|function|die
 DECL|function|error
 DECL|function|get_sha1_hex
 DECL|function|hexval
@@ -10,6 +11,7 @@ DECL|function|map_sha1_file
 DECL|function|read_cache
 DECL|function|read_sha1_file
 DECL|function|remove_file_from_cache
+DECL|function|report
 DECL|function|sha1_file_name
 DECL|function|sha1_to_hex
 DECL|function|unpack_sha1_file
