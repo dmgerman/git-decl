a15feb01aef0946df0acd00e19a70f5df1d34a68
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-15 22:53:45 -0700

    Make "read-tree" merge the trees it reads by giving them consecutive states.
    Normally you'd use state 0 for the "merged" state, and start out with
    state 1 being "origin", state 2 being "first tree" and state 3 being
    "second tree".
    
    Once all the index entries are back in state 0, we have a successful
    merge and can write the result tree back.

--- read-tree.c
+++ read-tree.c
@@ -2,4 +2,7 @@ DECL|function|main
 DECL|function|read_one_entry
 DECL|function|read_tree
 DECL|function|remove_lock_file
+DECL|function|same_entry
+DECL|function|trivially_merge_cache
 DECL|variable|remove_lock
+DECL|variable|stage
