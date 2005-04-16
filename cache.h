06078bbf6f8cb22a8fe03a334d93a21837cd3167
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-15 21:45:38 -0700

    Encode a few extra flags per index entry.
    This will allow us to have the same name in different "states" in the
    index at the same time. Which in turn seems to be a very simple way to
    merge.

--- cache.h
+++ cache.h
@@ -1,5 +1,8 @@
 DECL|macro|CACHE_H
 DECL|macro|CACHE_SIGNATURE
+DECL|macro|CE_NAMEMASK
+DECL|macro|CE_STAGE1
+DECL|macro|CE_STAGE2
 DECL|macro|CTIME_CHANGED
 DECL|macro|DATA_CHANGED
 DECL|macro|DB_ENVIRONMENT
@@ -14,11 +17,11 @@ DECL|macro|ce_namelen
 DECL|macro|ce_size
 DECL|member|ce_ctime
 DECL|member|ce_dev
+DECL|member|ce_flags
 DECL|member|ce_gid
 DECL|member|ce_ino
 DECL|member|ce_mode
 DECL|member|ce_mtime
-DECL|member|ce_namelen
 DECL|member|ce_size
 DECL|member|ce_uid
 DECL|member|hdr_entries
