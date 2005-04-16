b3491fa5430690214be29a4638923a36b3f773d9
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-16 09:11:49 -0700

    Make 'read-tree' do a few more of the trivial merge cases.
    This cuts down the work for the "real merge" to stuff where
    people might actually disagree on the algorithm. The trivial
    cases would seem to be totally independent of any policy.

--- read-tree.c
+++ read-tree.c
@@ -1,8 +1,9 @@
 DECL|function|main
+DECL|function|merge_entries
 DECL|function|read_one_entry
 DECL|function|read_tree
 DECL|function|remove_lock_file
-DECL|function|same_entry
+DECL|function|same
 DECL|function|trivially_merge_cache
 DECL|variable|remove_lock
 DECL|variable|stage
