384086a4721f448fd45acd192500de06b8036920
Junio C Hamano <junkio@cox.net>
2005-04-16 21:29:45 -0700

    [PATCH] show-diff shell safety
    The command line for running "diff" command is built without
    taking shell metacharacters into account.  A malicious dircache
    entry "foo 2>bar" (yes, a filename with space) would result in
    creating a file called "bar" with the error message "diff: foo:
    No such file or directory" in it.
    
    This is not just a user screwing over himself.  Such a dircache
    can be created as a result of a merge with tree from others.
    
    Here is a fix.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- show-diff.c
+++ show-diff.c
@@ -2,4 +2,6 @@ DECL|function|main
 DECL|function|matches_pathspec
 DECL|function|show_diff_empty
 DECL|function|show_differences
+DECL|function|sq_expand
+DECL|variable|diff_cmd
 DECL|variable|show_diff_usage
