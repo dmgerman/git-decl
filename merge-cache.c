67c6a7f9485d8e85a6c336b682c8e5c22dcd81ee
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-17 19:52:54 -0700

    Ass a "merge-cache" helper program to execute a merge on any unmerged files.
    This one doesn't actually do the merging, but it makes it
    easy to script the programs that do using it.

---
+++ merge-cache.c
@@ -0,0 +1,8 @@
+DECL|function|create_temp_file
+DECL|function|main
+DECL|function|merge_all
+DECL|function|merge_entry
+DECL|function|merge_file
+DECL|function|run_program
+DECL|variable|arguments
+DECL|variable|pgm
