a930261d599a5523cf03183ad86c05618ab51ab1
Junio C Hamano <junkio@cox.net>
2005-04-18 15:07:24 -0700

    [PATCH] show-diff.c: make diff options customizable.
    This makes the diff output formatting options customizable via the
    environment variables.  The default is still the Linux kernel style.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- show-diff.c
+++ show-diff.c
@@ -1,7 +1,9 @@
 DECL|function|main
 DECL|function|matches_pathspec
+DECL|function|prepare_diff_cmd
 DECL|function|show_diff_empty
 DECL|function|show_differences
 DECL|function|sq_expand
 DECL|variable|diff_cmd
+DECL|variable|diff_opts
 DECL|variable|show_diff_usage
