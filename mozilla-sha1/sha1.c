ab6719d64b6e161e80d22ae7298cefae0740d0aa
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-21 12:33:22 -0700

    Add support for alternate SHA1 library implementations.
    This one includes the Mozilla SHA1 implementation sent in by Edgar Toernig.
    It's dual-licenced under MPL-1.1 or GPL, so in the context of git, we
    obviously use the GPL version.
    
    Side note: the Mozilla SHA1 implementation is about twice as fast as the
    default openssl one on my G5, but the default openssl one has optimized
    x86 assembly language on x86. So choose wisely.

---
+++ mozilla-sha1/sha1.c
@@ -0,0 +1,5 @@
+DECL|function|SHA1_Final
+DECL|function|SHA1_Init
+DECL|function|SHA1_Update
+DECL|function|shaHashBlock
+DECL|macro|SHA_ROT
---
+++ mozilla-sha1/sha1.h
@@ -0,0 +1,6 @@
+DECL|member|H
+DECL|member|W
+DECL|member|lenW
+DECL|member|sizeHi
+DECL|member|sizeLo
+DECL|typedef|SHA_CTX
