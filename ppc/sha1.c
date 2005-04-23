0d8e82bd3fc43cfefe0e8218cc2336cfb53dd770
Paul Mackerras <paulus@samba.org>
2005-04-22 23:08:43 -0700

    [PATCH] PPC assembly implementation of SHA1
    Here is a SHA1 implementation with the core written in PPC assembly.
    On my 2GHz G5, it does 218MB/s, compared to 135MB/s for the openssl
    version or 45MB/s for the mozilla version.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ ppc/sha1.c
@@ -0,0 +1,3 @@
+DECL|function|SHA1_Final
+DECL|function|SHA1_Init
+DECL|function|SHA1_Update
---
+++ ppc/sha1.h
@@ -0,0 +1,8 @@
+DECL|member|b
+DECL|member|buf
+DECL|member|cnt
+DECL|member|hash
+DECL|member|l
+DECL|member|len
+DECL|struct|sha_context
+DECL|typedef|SHA_CTX
