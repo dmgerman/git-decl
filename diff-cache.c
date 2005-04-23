610b10a6dcac041c263c1c21a711c2548a364813
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-22 17:15:28 -0700

    New "diff-cache" implementation.
    This one is about a million times simpler, and much more likely to be
    correct too.
    
    Instead of trying to match up a tree object against the index, we just
    read in the tree object side-by-side into the index, and just walk the
    resulting index file. This was what all the read-tree cleanups were
    all getting to.

--- diff-cache.c
+++ diff-cache.c
@@ -1,12 +1,8 @@
-DECL|function|compare_tree_entry
 DECL|function|diff_cache
-DECL|function|extract
 DECL|function|main
-DECL|function|malloc_base
+DECL|function|remove_merge_entries
 DECL|function|show_file
-DECL|function|show_tree
-DECL|function|update_tree_entry
+DECL|function|show_modified
 DECL|variable|cached_only
 DECL|variable|diff_cache_usage
 DECL|variable|line_termination
-DECL|variable|recursive
