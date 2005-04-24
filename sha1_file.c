cdb2d80f8a447c7c6d1384b601e6864c6844ca9b
Daniel Barkalow <barkalow@iabervon.org>
2005-04-23 18:47:23 -0700

    [PATCH] Additional functions for the objects database
    This adds two functions: one to check if an object is present in the local
    database, and one to add an object to the local database by reading it
    from a file descriptor and checking its hash.
    
    Signed-Off-By: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- sha1_file.c
+++ sha1_file.c
@@ -1,6 +1,7 @@
 DECL|function|check_sha1_signature
 DECL|function|collision_check
 DECL|function|get_sha1_hex
+DECL|function|has_sha1_file
 DECL|function|hexval
 DECL|function|map_sha1_file
 DECL|function|read_sha1_file
@@ -10,6 +11,7 @@ DECL|function|sha1_to_hex
 DECL|function|unpack_sha1_file
 DECL|function|write_sha1_buffer
 DECL|function|write_sha1_file
+DECL|function|write_sha1_from_fd
 DECL|macro|O_NOATIME
 DECL|macro|O_NOATIME
 DECL|variable|sha1_file_directory
