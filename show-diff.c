637e922715ce9f42bd54a0da6ebddba404d01813
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-26 17:17:36 -0700

    show-diff: match diff-tree and diff-cache output
    You'll need "diff-tree-helper" to show the full diff, but Junio is
    dead set on adding a "-p" argument to all three to avoid it. That's
    next..

--- show-diff.c
+++ show-diff.c
@@ -1,3 +1,8 @@
 DECL|function|main
 DECL|function|matches_pathspec
+DECL|function|show_file
+DECL|variable|line_termination
+DECL|variable|recursive
 DECL|variable|show_diff_usage
+DECL|variable|silent
+DECL|variable|silent_on_nonexisting_files
