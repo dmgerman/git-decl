e3532ad0a03307f9b311b8c66c2845f7aa2c2364
Junio C Hamano <junkio@cox.net>
2005-04-27 09:21:00 -0700

    [PATCH] Reactivate show-diff patch generation
    This uses the reworked diff interface to generate patches directly out
    of show-diff when -p is specified.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- show-diff.c
+++ show-diff.c
@@ -1,8 +1,10 @@
 DECL|function|main
 DECL|function|matches_pathspec
 DECL|function|show_file
+DECL|function|show_modified
+DECL|function|show_unmerge
+DECL|variable|generate_patch
 DECL|variable|line_termination
-DECL|variable|recursive
 DECL|variable|show_diff_usage
 DECL|variable|silent
 DECL|variable|silent_on_nonexisting_files
