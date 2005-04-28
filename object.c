f3a85debcb70ad93dbffa76239e94aeddf301da1
Daniel Barkalow <barkalow@iabervon.org>
2005-04-28 07:46:33 -0700

    [PATCH] Add function to parse an object of unspecified type (take 2)
    This adds a function that parses an object from the database when we have
    to look up its actual type. It also checks the hash of the file, due to
    its heritage as part of fsck-cache.
    
    Signed-Off-By: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- object.c
+++ object.c
@@ -3,6 +3,7 @@ DECL|function|created_object
 DECL|function|find_object
 DECL|function|lookup_object
 DECL|function|mark_reachable
+DECL|function|parse_object
 DECL|variable|nr_objs
 DECL|variable|obj_allocs
 DECL|variable|objs
