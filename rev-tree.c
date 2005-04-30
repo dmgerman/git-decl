3d2761a832ee1506312000ab40402702ce782d9f
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-30 16:26:56 -0700

    rev-tree.c: don't include unnecessary header files
    We used to have all these time-related issues, long gone now.

--- rev-tree.c
+++ rev-tree.c
@@ -3,7 +3,5 @@ DECL|function|main
 DECL|function|process_commit
 DECL|function|read_cache_file
 DECL|macro|MAX_COMMITS
-DECL|macro|_BSD_SOURCE
-DECL|macro|_XOPEN_SOURCE
 DECL|variable|basemask
 DECL|variable|show_edges
