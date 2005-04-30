380e48e1868a013318c109c53aed43c8228bae5b
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-30 13:19:56 -0700

    Make the date parsing accept pretty much any random crap.
    This date parser turns line-noise into a date. Cool.

--- date.c
+++ date.c
@@ -1,6 +1,13 @@
 DECL|function|datestamp
+DECL|function|match_alpha
+DECL|function|match_digit
+DECL|function|match_string
+DECL|function|match_tz
 DECL|function|my_mktime
 DECL|function|parse_date
-DECL|function|skipfws
+DECL|macro|NR_TZ
+DECL|member|name
+DECL|member|offset
 DECL|variable|month_names
+DECL|variable|timezone_names
 DECL|variable|weekday_names
