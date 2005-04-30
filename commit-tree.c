164ded9d300cf7ffd863e3e66637a5ee28c5473f
Edgar Toernig <froese@gmx.de>
2005-04-30 09:46:49 -0700

    [PATCH] Do date parsing by hand...
    ...since everything out there is either strange (libc mktime has issues
    with timezones) or introduces unnecessary dependencies for people (libcurl).
    
    This goes back to the old date parsing, but moves it out into a file of
    its own, and does the "struct tm" to "seconds since epoch" handling by
    hand.
    
    I grepped through the tz-database and it seems there's one "country"
    left that has non-60-minute DST: Lord Howe Island.  All others dropped
    that before 1970.

--- commit-tree.c
+++ commit-tree.c
@@ -2,7 +2,6 @@ DECL|function|add_buffer
 DECL|function|check_valid
 DECL|function|init_buffer
 DECL|function|main
-DECL|function|parse_date
 DECL|function|remove_special
 DECL|macro|BLOCKING
 DECL|macro|MAXPARENT
---
+++ date.c
@@ -0,0 +1,6 @@
+DECL|function|datestamp
+DECL|function|my_mktime
+DECL|function|parse_date
+DECL|function|skipfws
+DECL|variable|month_names
+DECL|variable|weekday_names
