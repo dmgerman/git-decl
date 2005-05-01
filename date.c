72b5cd39e9e52c93b4724681a673769aeebd3d8f
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-05-01 11:48:34 -0700

    date.c: allow even more varied time formats
    (and some added checks for truly non-sensical stuff)

--- date.c
+++ date.c
@@ -1,6 +1,8 @@
 DECL|function|datestamp
+DECL|function|is_date
 DECL|function|match_alpha
 DECL|function|match_digit
+DECL|function|match_multi_number
 DECL|function|match_string
 DECL|function|match_tz
 DECL|function|my_mktime
