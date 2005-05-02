77175ba5c0e8e1c827b527b40497052697874f78
Junio C Hamano <junkio@cox.net>
2005-05-01 21:10:04 -0700

    [PATCH] git-local-pull updates
    This is to be applied on top of the previous patch to add
    git-local-pull command.  In addition to the '-l' (attempt
    hardlink before anything else) and the '-s' (then attempt
    symlink) flags, it adds '-n' (do not fall back to file copy)
    flag.  Also it updates the comments.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- local-pull.c
+++ local-pull.c
@@ -3,6 +3,7 @@ DECL|function|main
 DECL|function|say
 DECL|variable|local_pull_usage
 DECL|variable|path
+DECL|variable|use_filecopy
 DECL|variable|use_link
 DECL|variable|use_symlink
 DECL|variable|verbose
