40a986c515cb983065bb8716b2f1e8d14605cd86
Junio C Hamano <junkio@cox.net>
2005-05-01 21:09:28 -0700

    [PATCH] Add git-local-pull.
    This adds the git-local-pull command as a smaller brother of
    http-pull and rpull.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ local-pull.c
@@ -0,0 +1,8 @@
+DECL|function|fetch
+DECL|function|main
+DECL|function|say
+DECL|variable|local_pull_usage
+DECL|variable|path
+DECL|variable|use_link
+DECL|variable|use_symlink
+DECL|variable|verbose
