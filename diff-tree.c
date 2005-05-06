d85d44cd8a06fc559f74d696b1131e3f980f6b49
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-05-06 10:03:17 -0700

    diff-tree: support list if input trees on stdin
    This means that you can do
    
    	git-rev-list HEAD --max-count=10 | git-diff-tree --stdin update-cache.c
    
    to see which (if any) of the last ten commits changed update-cache.c.
    
    Use the "-m" flag to see merges too. Normally they are suppressed.

--- diff-tree.c
+++ diff-tree.c
@@ -1,6 +1,7 @@
 DECL|function|compare_tree_entry
 DECL|function|diff_tree
 DECL|function|diff_tree_sha1
+DECL|function|diff_tree_stdin
 DECL|function|extract
 DECL|function|interesting
 DECL|function|main
@@ -10,8 +11,10 @@ DECL|function|show_tree
 DECL|function|update_tree_entry
 DECL|variable|diff_tree_usage
 DECL|variable|generate_patch
+DECL|variable|ignore_merges
 DECL|variable|line_termination
 DECL|variable|nr_paths
 DECL|variable|pathlens
 DECL|variable|paths
+DECL|variable|read_stdin
 DECL|variable|recursive
