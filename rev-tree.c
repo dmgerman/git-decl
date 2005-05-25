060768f986a7a8581157efd13126dd50dd571099
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-05-24 21:34:12 -0700

    git-rev-tree: teach it about tag references
    And various cleanups that makes it able to not care.

--- rev-tree.c
+++ rev-tree.c
@@ -1,7 +1,8 @@
 DECL|function|interesting
 DECL|function|main
-DECL|function|process_commit
 DECL|function|read_cache_file
 DECL|macro|MAX_COMMITS
+DECL|macro|REACHABLE
+DECL|macro|cmit_flags
 DECL|variable|basemask
 DECL|variable|show_edges
