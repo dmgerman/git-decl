c9a74c7c5f87b27dd2b3395496ac60e54d217547
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-05-30 18:46:32 -0700

    git-rev-list: use proper lazy reachability analysis
    This mean sthat you can give a beginning/end pair to git-rev-list,
    and it will show all entries that are reachable from the beginning
    but not the end.
    
    For example
    
    	git-rev-list v2.6.12-rc5 v2.6.12-rc4
    
    shows all commits that are in -rc5 but are not in -rc4.

--- rev-list.c
+++ rev-list.c
@@ -1,2 +1,7 @@
+DECL|function|everybody_uninteresting
 DECL|function|main
+DECL|function|mark_parents_uninteresting
+DECL|macro|INTERESTING
+DECL|macro|SEEN
+DECL|macro|UNINTERESTING
 DECL|variable|rev_list_usage
