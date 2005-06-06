3784f5b55294e2d47b9b95095037c8115ea22e5b
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-06-05 20:02:31 -0700

    git-read-tree: be a lot more careful about merging dirty trees
    We don't want to overwrite state that we haven't committed yet
    when merging, so it's better to make git-read-tree fail than
    end up with a merge tree that ends up not having the dirty changes.
    
    Update git-resolve-script to fail cleanly when git-read-tree fails.

--- read-tree.c
+++ read-tree.c
@@ -1,11 +1,14 @@
 DECL|function|main
 DECL|function|merge_entries
 DECL|function|merge_stat_info
+DECL|function|old_match
 DECL|function|path_matches
 DECL|function|remove_lock_file
 DECL|function|same
 DECL|function|trivially_merge_cache
 DECL|function|unpack_tree
+DECL|function|verify_cleared
+DECL|function|verify_uptodate
 DECL|variable|lockfile_name
 DECL|variable|read_tree_usage
 DECL|variable|stage
