48a79abc2e9862247d5b2cc5fad56f7e1347a931
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-06-19 10:40:10 -0700

    git-commit-tree: ignore duplicate parents
    But warn about them.  If somebody really ends up later wanting to
    explicitly add a note that something has the same parent twice (who
    knows, there are strange people around), we can add a flag to say that
    it's expected and ok.
    
    This was brought on by a commit in the kernel tree, where a repeated
    merge caused a duplicate parent.
    
    Parent duplicates aren't "wrong" per se, they're just in practice not
    something you are ever interested in.

--- commit-tree.c
+++ commit-tree.c
@@ -2,7 +2,9 @@ DECL|function|add_buffer
 DECL|function|check_valid
 DECL|function|init_buffer
 DECL|function|main
+DECL|function|new_parent
 DECL|function|remove_special
 DECL|macro|BLOCKING
 DECL|macro|MAXPARENT
 DECL|variable|commit_tree_usage
+DECL|variable|parent_sha1
