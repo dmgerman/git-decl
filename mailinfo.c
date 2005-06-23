8f2751aa6c5af7bb2d49ca6e228f7cb140b97d63
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-06-23 09:40:23 -0700

    Avoid doing the "filelist" thing, since "git-apply" picks up the files automatically
    ..and git-apply does a lot better job at it anyway.
    
    Also, we break the comment/diff on a line that starts with "diff -", not
    just on the "---" line.  Especially for git diffs, we actually want that
    line in the diff.
    
    (We should probably also break on "Index: ..." followed by "=====")

--- mailinfo.c
+++ mailinfo.c
@@ -10,12 +10,10 @@ DECL|function|handle_rest
 DECL|function|handle_subject
 DECL|function|main
 DECL|function|sanity_check
-DECL|function|show_filename
 DECL|function|usage
 DECL|variable|cmitmsg
 DECL|variable|date
 DECL|variable|email
-DECL|variable|filelist
 DECL|variable|line
 DECL|variable|name
 DECL|variable|patchfile
