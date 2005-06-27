871e978ce266b97218fe25b67732aee47d3bf9e0
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-06-26 20:27:56 -0700

    git-pack-objects: write the pack files with a SHA1 csum
    We want to be able to check their integrity later, and putting the
    sha1-sum of the contents at the end is a good thing.  The writing
    routines are generic, so we could try to re-use them for the index file,
    instead of having the same logic duplicated.
    
    Update unpack-objects to know about the extra 20 bytes at the end
    of the index.

---
+++ csum-file.c
@@ -0,0 +1,5 @@
+DECL|function|sha1close
+DECL|function|sha1create
+DECL|function|sha1flush
+DECL|function|sha1write
+DECL|function|sha1write_compressed
---
+++ csum-file.h
@@ -0,0 +1,7 @@
+DECL|macro|CSUM_FILE_H
+DECL|member|buffer
+DECL|member|ctx
+DECL|member|error
+DECL|member|fd
+DECL|member|offset
+DECL|struct|sha1file
