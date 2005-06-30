b87d1444635414551877d1f3e40e151cf9890f52
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-06-29 20:50:15 -0700

    Make send/receive-pack be closer to doing something interesting

---
+++ pkt-line.c
@@ -0,0 +1,6 @@
+DECL|function|packet_flush
+DECL|function|packet_read_line
+DECL|function|packet_write
+DECL|function|safe_read
+DECL|function|safe_write
+DECL|macro|hex
---
+++ pkt-line.h
@@ -0,0 +1,1 @@
+DECL|macro|PKTLINE_H
