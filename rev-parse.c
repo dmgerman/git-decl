4ed63f06bebda567a75d1caee68961370bb0e592
Linus Torvalds <torvalds@g5.osdl.org>
2005-07-03 20:27:06 -0700

    Make git-rev-parse support cogito-style "short hex names"
    Currently only for unpacked objects, but the infrastructure
    is there to do it for packed objects too.

--- rev-parse.c
+++ rev-parse.c
@@ -1,5 +1,8 @@
+DECL|function|find_short_object_filename
+DECL|function|find_short_packed_object
 DECL|function|get_extended_sha1
 DECL|function|get_parent
+DECL|function|get_short_sha1
 DECL|function|is_rev_argument
 DECL|function|main
 DECL|function|show_arg
