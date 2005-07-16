17126d5fb70afb660b05e9906aa272e69d4c965a
Linus Torvalds <torvalds@g5.osdl.org>
2005-07-16 09:57:03 -0700

    git-convert-cache: fix up file modes in trees too
    git-fsck-cache complains about some of the odder ones, and is quiet
    about the old (S_IFREG | 664) case, but that's wrong too.
    
    Converting the kernel tree is too painful right now, but at least we
    know how to do it if we ever want to.

--- convert-cache.c
+++ convert-cache.c
@@ -4,6 +4,7 @@ DECL|function|convert_commit
 DECL|function|convert_date
 DECL|function|convert_date_line
 DECL|function|convert_entry
+DECL|function|convert_mode
 DECL|function|convert_tree
 DECL|function|insert_new
 DECL|function|lookup_entry
