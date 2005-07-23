9d50de22daf982b776d3dc6b3ec31d841b91cc64
Junio C Hamano <junkio@cox.net>
2005-07-22 19:08:32 -0700

    [PATCH] Help scripts that use git-rev-parse to grok args with SP/TAB/LF
    The git-rev-parse command uses LF to separate each argument it
    parses, so its users at least need to set IFS to LF to be able
    to handle filenames with embedded SPs and TABs.  Some commands,
    however, can take and do expect arguments with embedded LF,
    notably, "-S" (pickaxe) of diff family, so even this workaround
    does not work for them.
    
    When --sq flag to git-rev-parse is given, instead of showing one
    argument per line, it outputs arguments quoted for consumption
    with "eval" by the caller, to remedy this situation.
    
    As an example, this patch converts git-whatchanged to use this
    new feature.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- rev-parse.c
+++ rev-parse.c
@@ -6,6 +6,7 @@ DECL|function|get_short_sha1
 DECL|function|is_rev_argument
 DECL|function|main
 DECL|function|match_sha
+DECL|function|show
 DECL|function|show_arg
 DECL|function|show_default
 DECL|function|show_norev
@@ -20,6 +21,7 @@ DECL|variable|flags_only
 DECL|variable|no_flags
 DECL|variable|no_revs
 DECL|variable|output_revs
+DECL|variable|output_sq
 DECL|variable|revs_only
 DECL|variable|show_type
 DECL|variable|single_rev
