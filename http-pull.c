c8d02201761cc887b093d72fd9c4ea8b40733e71
barkalow@iabervon.org <barkalow@iabervon.org>
2005-07-31 20:54:17 -0400

    [PATCH] Support downloading packs by HTTP (whitespace fixed)
    This adds support to http-pull for finding the list of pack files
    available on the server, downloading the index files for those pack
    files, and downloading pack files when they contain needed objects not
    available individually. It retains the index files even if the pack
    files were not needed, but downloads the list of pack files once per
    run if an object is not found separately.
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- http-pull.c
+++ http-pull.c
@@ -1,8 +1,12 @@
 DECL|function|fetch
+DECL|function|fetch_index
+DECL|function|fetch_indices
+DECL|function|fetch_pack
 DECL|function|fetch_ref
 DECL|function|fwrite_buffer
 DECL|function|fwrite_sha1_file
 DECL|function|main
+DECL|function|setup_index
 DECL|macro|curl_global_cleanup
 DECL|macro|curl_global_init
 DECL|member|buffer
@@ -13,6 +17,8 @@ DECL|variable|base
 DECL|variable|c
 DECL|variable|curl
 DECL|variable|curl_ssl_verify
+DECL|variable|got_indices
 DECL|variable|local
+DECL|variable|packs
 DECL|variable|stream
 DECL|variable|zret
