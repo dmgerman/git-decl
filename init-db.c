f39862fbc0cd3032ec6953fac8596d87f54fe367
Junio C Hamano <junkio@cox.net>
2005-08-06 12:50:14 -0700

    Redo the templates generation and installation.
    Per discussion with people interested in binary packaging,
    change the default template location from /etc/git-core to
    /usr/share/git-core hierarchy.  If a user wants to run git
    before installing for whatever reason, in addition to adding
    $src to the PATH environment variable, git-init-db can be run
    with --template=$src/templates/blt/ parameter.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- init-db.c
+++ init-db.c
@@ -4,3 +4,5 @@ DECL|function|copy_templates_1
 DECL|function|create_default_files
 DECL|function|main
 DECL|function|safe_create_dir
+DECL|macro|DEFAULT_GIT_TEMPLATE_DIR
+DECL|variable|init_db_usage
