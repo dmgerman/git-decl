14392d81c7bcb9e66b090f6ba7c16da74ea4bc73
Junio C Hamano <junkio@cox.net>
2005-09-16 22:30:55 -0700

    Retire rev-tree.
    Some old scripts might still use git-rev-tree, but it really is
    clearly inferior in every way to git-rev-list that such scripts should
    be fixed anyway. Fixing them should be pretty easy.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- rev-tree.c
+++
@@ -1,8 +0,0 @@
-DECL|function|interesting
-DECL|function|main
-DECL|function|read_cache_file
-DECL|macro|MAX_COMMITS
-DECL|macro|REACHABLE
-DECL|macro|cmit_flags
-DECL|variable|basemask
-DECL|variable|show_edges
