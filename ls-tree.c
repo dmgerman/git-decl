e3b2e00653fa2cb816c4727a50fae697ea25215a
Linus Torvalds <torvalds@osdl.org>
2005-11-26 09:38:20 -0800

    ls-tree: major rewrite to do pathspec
    git-ls-tree should be rewritten to use a pathspec the same way everybody
    else does. Right now it's the odd man out: if you do
    
    	git-ls-tree HEAD divers/char drivers/
    
    it will show the same files _twice_, which is not how pathspecs in general
    work.
    
    How about this patch? It breaks some of the git-ls-tree tests, but it
    makes git-ls-tree work a lot more like other git pathspec commands, and it
    removes more than 150 lines by re-using the recursive tree traversal (but
    the "-d" flag is gone for good, so I'm not pushing this too hard).
    
    		Linus

--- ls-tree.c
+++ ls-tree.c
@@ -1,16 +1,7 @@
-DECL|function|entry_hex
-DECL|function|entry_type
-DECL|function|find_entry
-DECL|function|list
-DECL|function|list_one
 DECL|function|main
-DECL|function|prepare_children
-DECL|function|prepare_root
-DECL|function|show_children
-DECL|function|show_entry
+DECL|function|show_tree
 DECL|macro|LS_RECURSIVE
 DECL|macro|LS_TREE_ONLY
 DECL|variable|line_termination
 DECL|variable|ls_options
 DECL|variable|ls_tree_usage
-DECL|variable|root_entry
