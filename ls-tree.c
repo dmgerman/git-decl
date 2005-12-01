8f7ead563e58225f59ac4f052d09c7a0d130e3c8
Linus Torvalds <torvalds@osdl.org>
2005-12-01 10:35:51 -0800

    git-ls-tree: add "-t" option to always show the tree entries
    The old (new) behaviour was that it only shows trees if the object is
    specified exactly, and recursive is not set. That makes sense, because
    there is obviously nothing else it can show for that case.
    
    However, with the new "-t" option, it will show the tree even with "-r",
    as it traverses down into it.
    
    NOTE! This also means that it will show all trees leading up to that tree.
    
    For example, if you do a
    
    	git-ls-tree -t HEAD -- drivers/char/this/file/does/not/exist
    
    it will show the trees that lead up to the files that do not exist:
    
    	[torvalds@g5 linux]$ git-ls-tree -t HEAD -- drivers/char/this/file/does/not/exist
    	040000 tree 9cb687b77dcd64bf82e9a73214db467c964c1266    drivers
    	040000 tree 298e2fadf0ff3867d1ef49936fd2c7bf6ce1eb66    drivers/char
    	[torvalds@g5 linux]$
    
    and note how this is true even though I didn't specify "-r": the fact that
    I supplied a pathspec automatically implies "enough recursion" for that
    particular pathspec.
    
    I think the code is cleaner and easier to understand too: the patch looks
    bigger, but it's really just splitting up the "should we recurse into this
    tree" into a function of its own.
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- ls-tree.c
+++ ls-tree.c
@@ -1,6 +1,8 @@
 DECL|function|main
+DECL|function|show_recursive
 DECL|function|show_tree
 DECL|macro|LS_RECURSIVE
+DECL|macro|LS_SHOW_TREES
 DECL|macro|LS_TREE_ONLY
 DECL|variable|line_termination
 DECL|variable|ls_options
