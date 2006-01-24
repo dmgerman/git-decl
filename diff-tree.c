1d4bf57d9c20ad8df6c41034c6b763e3d67917f6
Junio C Hamano <junkio@cox.net>
2006-01-24 01:22:04 -0800

    diff-tree -c: show a merge commit a bit more sensibly.
    A new option '-c' to diff-tree changes the way a merge commit is
    displayed when generating a patch output.  It shows a "combined
    diff" (hence the option letter 'c'), which looks like this:
    
        $ git-diff-tree --pretty -c -p fec9ebf1 | head -n 18
        diff-tree fec9ebf... (from parents)
        Merge: 0620db3... 8a263ae...
        Author: Junio C Hamano <junkio@cox.net>
        Date:   Sun Jan 15 22:25:35 2006 -0800
    
    	Merge fixes up to GIT 1.1.3
    
        diff --combined describe.c
        @@@ +98,7 @@@
    	    return (a_date > b_date) ? -1 : (a_date == b_date) ? 0 : 1;
           }
    
        -  static void describe(char *arg)
         - static void describe(struct commit *cmit, int last_one)
        ++ static void describe(char *arg, int last_one)
           {
         +      unsigned char sha1[20];
         +      struct commit *cmit;
    
    There are a few things to note about this feature:
    
     - The '-c' option implies '-p'.  It also implies '-m' halfway
       in the sense that "interesting" merges are shown, but not all
       merges.
    
     - When a blob matches one of the parents, we do not show a diff
       for that path at all.  For a merge commit, this option shows
       paths with real file-level merge (aka "interesting things").
    
     - As a concequence of the above, an "uninteresting" merge is
       not shown at all.  You can use '-m' in addition to '-c' to
       show the commit log for such a merge, but there will be no
       combined diff output.
    
     - Unlike "gitk", the output is monochrome.
    
    A '-' character in the nth column means the line is from the nth
    parent and does not appear in the merge result (i.e. removed
    from that parent's version).
    
    A '+' character in the nth column means the line appears in the
    merge result, and the nth parent does not have that line
    (i.e. added by the merge itself or inherited from another
    parent).
    
    The above example output shows that the function signature was
    changed from either parents (hence two "-" lines and a "++"
    line), and "unsigned char sha1[20]", prefixed by a " +", was
    inherited from the first parent.
    
    The code as sent to the list was buggy in few corner cases,
    which I have fixed since then.
    
    It does not bother to keep track of and show the line numbers
    from parent commits, which it probably should.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- diff-tree.c
+++ diff-tree.c
@@ -5,6 +5,7 @@ DECL|function|diff_tree_sha1_top
 DECL|function|diff_tree_stdin
 DECL|function|generate_header
 DECL|function|main
+DECL|variable|combine_merges
 DECL|variable|commit_format
 DECL|variable|diff_options
 DECL|variable|diff_tree_usage
@@ -13,5 +14,6 @@ DECL|variable|header_prefix
 DECL|variable|ignore_merges
 DECL|variable|no_commit_id
 DECL|variable|read_stdin
+DECL|variable|show_empty_combined
 DECL|variable|show_root_diff
 DECL|variable|verbose_header
