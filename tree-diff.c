8379cdd5e9f3d98be3022865c7f0e6dc03bea354
Linus Torvalds <torvalds@osdl.org>
2006-01-31 14:10:56 -0800

    Make the "struct tree_desc" operations available to others
    We have operations to "extract" and "update" a "struct tree_desc", but we
    only used them in tree-diff.c and they were static to that file.
    
    But other tree traversal functions can use them to their advantage
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- tree-diff.c
+++ tree-diff.c
@@ -3,11 +3,11 @@ DECL|function|count_paths
 DECL|function|diff_tree
 DECL|function|diff_tree_setup_paths
 DECL|function|diff_tree_sha1
-DECL|function|extract
 DECL|function|interesting
 DECL|function|malloc_base
 DECL|function|show_entry
 DECL|function|show_tree
+DECL|function|tree_entry_extract
 DECL|function|update_tree_entry
 DECL|variable|nr_paths
 DECL|variable|pathlens
