17bdc51df2b33d10dc71dc64bd3e3b82122f348f
Linus Torvalds <torvalds@osdl.org>
2006-02-25 16:19:46 -0800

    First cut at libifying revlist generation
    This really just splits things up partially, and creates the
    interface to set things up by parsing the command line.
    
    No real code changes so far, although the parsing of filenames is a bit
    stricter. In particular, if there is a "--", then we do not accept any
    filenames before it, and if there isn't any "--", then we check that _all_
    paths listed are valid, not just the first one.
    
    The new argument parsing automatically also gives us "--default" and
    "--not" handling as in git-rev-parse.
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

---
+++ revision.c
@@ -0,0 +1,13 @@
+DECL|function|add_object
+DECL|function|add_one_commit
+DECL|function|add_pending_object
+DECL|function|get_commit_reference
+DECL|function|handle_all
+DECL|function|handle_one_ref
+DECL|function|mark_blob_uninteresting
+DECL|function|mark_parents_uninteresting
+DECL|function|mark_tree_uninteresting
+DECL|function|path_name
+DECL|function|setup_revisions
+DECL|variable|all_flags
+DECL|variable|all_revs
