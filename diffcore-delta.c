05b7a91e9e934fca152892f8142f68dd79c386c7
Junio C Hamano <junkio@cox.net>
2006-02-28 20:16:41 -0800

    diffcore-delta: stop using deltifier for packing.
    This switches the change estimation logic used by break, rename
    and copy detection from delta packing code to a more line
    oriented one.  This way, thee performance-density tradeoff by
    delta packing code can be made without worrying about breaking
    the rename detection.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- diffcore-delta.c
+++ diffcore-delta.c
@@ -1,2 +1,7 @@
 DECL|function|diffcore_count_changes
-DECL|function|diffcore_count_changes_1
+DECL|function|hash_extended_line
+DECL|function|hash_lines
+DECL|function|linehash_compare
+DECL|member|bytes
+DECL|member|hash
+DECL|struct|linehash
