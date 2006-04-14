755c1679fd7aafed701f3577c6498b81b25ff3ed
Linus Torvalds <torvalds@osdl.org>
2006-04-14 16:52:13 -0700

    Common option parsing for "git log --diff" and friends
    This basically does a few things that are sadly somewhat interdependent,
    and nontrivial to split out
    
     - get rid of "struct log_tree_opt"
    
       The fields in "log_tree_opt" are moved into "struct rev_info", and all
       users of log_tree_opt are changed to use the rev_info struct instead.
    
     - add the parsing for the log_tree_opt arguments to "setup_revision()"
    
     - make setup_revision set a flag (revs->diff) if the diff-related
       arguments were used. This allows "git log" to decide whether it wants
       to show diffs or not.
    
     - make setup_revision() also initialize the diffopt part of rev_info
       (which we had from before, but we just didn't initialize it)
    
     - make setup_revision() do all the "finishing touches" on it all (it will
       do the proper flag combination logic, and call "diff_setup_done()")
    
    Now, that was the easy and straightforward part.
    
    The slightly more involved part is that some of the programs that want to
    use the new-and-improved rev_info parsing don't actually want _commits_,
    they may want tree'ish arguments instead. That meant that I had to change
    setup_revision() to parse the arguments not into the "revs->commits" list,
    but into the "revs->pending_objects" list.
    
    Then, when we do "prepare_revision_walk()", we walk that list, and create
    the sorted commit list from there.
    
    This actually cleaned some stuff up, but it's the less obvious part of the
    patch, and re-organized the "revision.c" logic somewhat. It actually paves
    the way for splitting argument parsing _entirely_ out of "revision.c",
    since now the argument parsing really is totally independent of the commit
    walking: that didn't use to be true, since there was lots of overlap with
    get_commit_reference() handling etc, now the _only_ overlap is the shared
    (and trivial) "add_pending_object()" thing.
    
    However, I didn't do that file split, just because I wanted the diff
    itself to be smaller, and show the actual changes more clearly. If this
    gets accepted, I'll do further cleanups then - that includes the file
    split, but also using the new infrastructure to do a nicer "git diff" etc.
    
    Even in this form, it actually ends up removing more lines than it adds.
    
    It's nice to note how simple and straightforward this makes the built-in
    "git log" command, even though it continues to support all the diff flags
    too. It doesn't get much simpler that this.
    
    I think this is worth merging soonish, because it does allow for future
    cleanup and even more sharing of code. However, it obviously touches
    "revision.c", which is subtle. I've tested that it passes all the tests we
    have, and it passes my "looks sane" detector, but somebody else should
    also give it a good look-over.
    
    [jc: squashed the original and three "oops this too" updates, with
     another fix-up.]
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- log-tree.c
+++ log-tree.c
@@ -1,7 +1,5 @@
 DECL|function|diff_root_tree
 DECL|function|do_diff_combined
 DECL|function|generate_header
-DECL|function|init_log_tree_opt
 DECL|function|log_tree_commit
 DECL|function|log_tree_diff_flush
-DECL|function|log_tree_opt_parse
