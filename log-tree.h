3ae0f969500cf431317ddc2db023a230b839c8e3
Junio C Hamano <junkio@cox.net>
2006-04-15 01:59:40 -0700

    Revert all the rev-list option parsing changes.
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- log-tree.h
+++ log-tree.h
@@ -1,1 +1,13 @@
 DECL|macro|LOG_TREE_H
+DECL|member|always_show_header
+DECL|member|combine_merges
+DECL|member|commit_format
+DECL|member|dense_combined_merges
+DECL|member|diffopt
+DECL|member|header
+DECL|member|header_prefix
+DECL|member|ignore_merges
+DECL|member|no_commit_id
+DECL|member|show_root_diff
+DECL|member|verbose_header
+DECL|struct|log_tree_opt
