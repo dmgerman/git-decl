4ea75aac20dca6d1cd6ee00b37a96189a1e08ba8
Junio C Hamano <junkio@cox.net>
2006-04-19 23:52:05 -0700

    git-update-index --unresolve
    Retire git-unresolve and make it into "git-update-index --unresolve".
    It processes all paths that follow.
    
    During a merge, you would mark a path that is dealt with with:
    
    	$ git update-index hello
    
    and you would "undo" it with:
    
    	$ git update-index --unresolve hello
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- unresolve.c
+++
@@ -1,8 +0,0 @@
-DECL|function|main
-DECL|function|read_head_pointers
-DECL|function|read_one_ent
-DECL|function|unresolve_one
-DECL|variable|cache_file
-DECL|variable|head_sha1
-DECL|variable|merge_head_sha1
-DECL|variable|unresolve_usage
