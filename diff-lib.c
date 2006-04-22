c7a115502686742d09d8af2959d84d36d6a2484a
Junio C Hamano <junkio@cox.net>
2006-04-22 02:43:00 -0700

    Libify diff-index.
    The second installment to libify diff brothers.  The pathname
    arguments are checked more strictly than before because we now
    use the revision.c::setup_revisions() infrastructure.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- diff-lib.c
+++ diff-lib.c
@@ -1,1 +1,8 @@
+DECL|function|diff_cache
+DECL|function|diff_index_show_file
+DECL|function|get_stat_data
+DECL|function|mark_merge_entries
 DECL|function|run_diff_files
+DECL|function|run_diff_index
+DECL|function|show_modified
+DECL|function|show_new_file
