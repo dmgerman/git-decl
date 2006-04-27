f4a5ddb98017a892c1a0fd365e9769d2516f9fcb
Junio C Hamano <junkio@cox.net>
2006-04-27 12:48:26 -0700

    Retire rabinpoly fingerprinting code
    For now let's retire this and reintroduce it as part of the updated
    pack-objects series from Geert when it is ready.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- gsimm.c
+++
@@ -1,6 +0,0 @@
-DECL|function|freq_to_md
-DECL|function|gb_simm_process
-DECL|macro|DUP_CACHE_SIZE
-DECL|macro|GROUP_BITS
-DECL|macro|GROUP_COUNTERS
-DECL|macro|NUM_HASHES_PER_CHAR
--- rabinpoly.h
+++
@@ -1,1 +0,0 @@
-DECL|macro|RABIN_WINDOW_SIZE
