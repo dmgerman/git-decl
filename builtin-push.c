e24093865be0c7074e0933df917334dfc8d31a47
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-04-30 14:05:55 +0200

    builtin-push: resurrect parsing of Push: lines
    The C'ification of push left these behind.
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-push.c
+++ builtin-push.c
@@ -8,13 +8,16 @@ DECL|function|get_remotes_uri
 DECL|function|get_uri
 DECL|function|set_refspecs
 DECL|macro|BUF_SIZE
+DECL|macro|MAX_REFSPECS
 DECL|macro|MAX_URI
 DECL|variable|all
 DECL|variable|buffer
+DECL|variable|current_refspec
 DECL|variable|execute
 DECL|variable|force
 DECL|variable|push_usage
 DECL|variable|refspec
 DECL|variable|refspec_nr
+DECL|variable|refspecs_
 DECL|variable|tags
 DECL|variable|thin
