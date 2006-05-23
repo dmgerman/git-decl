809de3907307bc145ec6a9881edb26baa94bd00b
Peter Eriksen <s022018@student.dtu.dk>
2006-05-23 14:15:29 +0200

    Builtin git-ls-files.
    Signed-off-by: Peter Eriksen <s022018@student.dtu.dk>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-ls-files.c
+++ builtin-ls-files.c
@@ -2,11 +2,11 @@ DECL|function|add_exclude
 DECL|function|add_excludes_from_file
 DECL|function|add_excludes_from_file_1
 DECL|function|add_name
+DECL|function|cmd_ls_files
 DECL|function|cmp_name
 DECL|function|dir_exists
 DECL|function|excluded
 DECL|function|excluded_1
-DECL|function|main
 DECL|function|match
 DECL|function|pop_exclude_per_directory
 DECL|function|prune_cache
