6e38ea484f58a229ee1b300368acf7805f8177e1
Petr Baudis <pasky@suse.cz>
2006-06-24 04:34:38 +0200

    Customizable error handlers
    This patch makes the usage(), die() and error() handlers customizable.
    Nothing in the git code itself uses that but many other libgit users
    (like Git.pm) will.
    
    This is implemented using the mutator functions primarily because you
    cannot directly modifying global variables of libgit from a program that
    dlopen()ed it, apparently. But having functions for that is a better API
    anyway.
    
    Signed-off-by: Petr Baudis <pasky@suse.cz>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- usage.c
+++ usage.c
@@ -1,4 +1,11 @@
 DECL|function|die
+DECL|function|die_builtin
 DECL|function|error
+DECL|function|error_builtin
 DECL|function|report
+DECL|function|set_die_routine
+DECL|function|set_error_routine
+DECL|function|set_usage_routine
 DECL|function|usage
+DECL|function|usage_builtin
+DECL|variable|error_routine
