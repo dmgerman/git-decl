29943df9adcfa1ce57934775e35620ec5f06cf58
Shawn Pearce <spearce@spearce.org>
2006-08-23 02:49:00 -0400

    Convert memcpy(a,b,20) to hashcpy(a,b).
    This abstracts away the size of the hash values when copying them
    from memory location to memory location, much as the introduction
    of hashcmp abstracted away hash value comparsion.
    
    A few call sites were using char* rather than unsigned char* so
    I added the cast rather than open hashcpy to be void*.  This is a
    reasonable tradeoff as most call sites already use unsigned char*
    and the existing hashcmp is also declared to be unsigned char*.
    
    [jc: Splitted the patch to "master" part, to be followed by a
     patch for merge-recursive.c which is not in "master" yet.
    
     Fixed the cast in the latter hunk to combine-diff.c which was
     wrong in the original.
    
     Also converted ones left-over in combine-diff.c, diff-lib.c and
     upload-pack.c ]
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- cache.h
+++ cache.h
@@ -4,6 +4,7 @@ DECL|enumerator|PERM_UMASK
 DECL|enum|sharedrepo
 DECL|function|create_ce_mode
 DECL|function|hashcmp
+DECL|function|hashcpy
 DECL|function|is_null_sha1
 DECL|macro|ADD_CACHE_OK_TO_ADD
 DECL|macro|ADD_CACHE_OK_TO_REPLACE
