70852e53d419c2db5de7449b87042dfb5746d141
Shawn O. Pearce <spearce@spearce.org>
2006-08-23 04:31:12 -0400

    Added branch load counter to fast-import.
    If the branch load count exceeds the number of branches created then
    the frontend is causing fast-import to page branches into and out of
    memory due to the way its ordering its commits.  Performance can
    likely be increased if the frontend were to alter its commit
    sequence such that it stays on one branch before switching to another
    branch, then never returns to the prior branch.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- fast-import.c
+++ fast-import.c
@@ -102,6 +102,7 @@ DECL|variable|avail_tree_table
 DECL|variable|avail_tree_table_sz
 DECL|variable|blocks
 DECL|variable|branch_count
+DECL|variable|branch_load_count
 DECL|variable|branch_table
 DECL|variable|branch_table_sz
 DECL|variable|command_buf
