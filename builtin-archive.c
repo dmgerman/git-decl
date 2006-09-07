e50726eb130c6c2b76895147ec36d95e4b6846ab
Franck Bui-Huu <vagabon.xyz@gmail.com>
2006-09-07 15:12:02 +0200

    Add git-archive
    git-archive is a command to make TAR and ZIP archives of a git tree.
    It helps prevent a proliferation of git-{format}-tree commands.
    
    Instead of directly calling git-{tar,zip}-tree command, it defines
    a very simple API, that archiver should implement and register in
    "git-archive.c". This API is made up by 2 functions whose prototype
    is defined in "archive.h" file.
    
     - The first one is used to parse 'extra' parameters which have
       signification only for the specific archiver. That would allow
       different archive backends to have different kind of options.
    
     - The second one is used to ask to an archive backend to build
       the archive given some already resolved parameters.
    
    The main reason for making this API is to avoid using
    git-{tar,zip}-tree commands, hence making them useless. Maybe it's
    time for them to die ?
    
    It also implements remote operations by defining a very simple
    protocol: it first sends the name of the specific uploader followed
    the repository name (git-upload-tar git://example.org/repo.git).
    Then it sends options. It's done by sending a sequence of one
    argument per packet, with prefix "argument ", followed by a flush.
    
    The remote protocol is implemented in "git-archive.c" for client
    side and is triggered by "--remote=<repo>" option. For example,
    to fetch a TAR archive in a remote repo, you can issue:
    
    $ git archive --format=tar --remote=git://xxx/yyy/zzz.git HEAD
    
    We choose to not make a new command "git-fetch-archive" for example,
    avoind one more GIT command which should be nice for users (less
    commands to remember, keeps existing --remote option).
    
    Signed-off-by: Franck Bui-Huu <vagabon.xyz@gmail.com>
    Acked-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

---
+++ builtin-archive.c
@@ -0,0 +1,9 @@
+DECL|function|cmd_archive
+DECL|function|default_parse_extra
+DECL|function|init_archiver
+DECL|function|parse_archive_args
+DECL|function|parse_pathspec_arg
+DECL|function|parse_treeish_arg
+DECL|function|run_remote_archiver
+DECL|variable|archive_usage
+DECL|variable|archivers
