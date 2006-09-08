4e7909478cf819bc354e0be9df14844ac4fe6a31
Jeff King <peff@peff.net>
2006-09-08 04:05:34 -0400

    git-commit.sh: convert run_status to a C builtin
    This creates a new git-runstatus which should do roughly the same thing
    as the run_status function from git-commit.sh. Except for color support,
    the main focus has been to keep the output identical, so that it can be
    verified as correct and then used as a C platform for other improvements to
    the status printing code.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

---
+++ wt-status.c
@@ -0,0 +1,17 @@
+DECL|function|color
+DECL|function|git_status_config
+DECL|function|parse_status_slot
+DECL|function|wt_status_prepare
+DECL|function|wt_status_print
+DECL|function|wt_status_print_changed
+DECL|function|wt_status_print_changed_cb
+DECL|function|wt_status_print_filepair
+DECL|function|wt_status_print_header
+DECL|function|wt_status_print_initial
+DECL|function|wt_status_print_trailer
+DECL|function|wt_status_print_untracked
+DECL|function|wt_status_print_updated
+DECL|function|wt_status_print_updated_cb
+DECL|function|wt_status_print_verbose
+DECL|variable|wt_status_colors
+DECL|variable|wt_status_use_color
