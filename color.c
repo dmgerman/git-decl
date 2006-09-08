1b5a9bc87e308fae8024e9d18253aac0f0ce5c22
Jeff King <peff@peff.net>
2006-09-08 04:03:18 -0400

    Move color option parsing out of diff.c and into color.[ch]
    The intent is to lib-ify colorizing code so it can be reused.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

---
+++ color.c
@@ -0,0 +1,8 @@
+DECL|function|color_parse
+DECL|function|color_printf
+DECL|function|color_printf_ln
+DECL|function|color_vprintf
+DECL|function|git_config_colorbool
+DECL|function|parse_attr
+DECL|function|parse_color
+DECL|macro|COLOR_RESET
