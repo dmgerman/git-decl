64a5d7d9c857b6ee0be4fae7faccd8a48b3ab311
Linus Torvalds <torvalds@osdl.org>
2006-09-11 16:37:32 -0700

    Start handling references internally as a sorted in-memory list
    This also adds some very rudimentary support for the notion of packed
    refs.  HOWEVER! At this point it isn't used to actually look up a ref
    yet, only for listing them (ie "for_each_ref()" and friends see the
    packed refs, but none of the other single-ref lookup routines).
    
    Note how we keep two separate lists: one for the loose refs, and one for
    the packed refs we read. That's so that we can easily keep the two apart,
    and read only one set or the other (and still always make sure that the
    loose refs take precedence).
    
    [ From this, it's not actually obvious why we'd keep the two separate
      lists, but it's important to have the packed refs on their own list
      later on, when I add support for looking up a single loose one.
    
      For that case, we will want to read _just_ the packed refs in case the
      single-ref lookup fails, yet we may end up needing the other list at
      some point in the future, so keeping them separated is important ]
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- refs.c
+++ refs.c
@@ -1,3 +1,4 @@
+DECL|function|add_ref
 DECL|function|bad_ref_char
 DECL|function|check_ref_format
 DECL|function|create_symref
@@ -6,12 +7,16 @@ DECL|function|for_each_branch_ref
 DECL|function|for_each_ref
 DECL|function|for_each_remote_ref
 DECL|function|for_each_tag_ref
+DECL|function|get_loose_refs
+DECL|function|get_packed_refs
+DECL|function|get_ref_dir
 DECL|function|get_ref_sha1
 DECL|function|head_ref
 DECL|function|lock_any_ref_for_update
 DECL|function|lock_ref_sha1
 DECL|function|lock_ref_sha1_basic
 DECL|function|log_ref_write
+DECL|function|parse_ref_line
 DECL|function|read_ref
 DECL|function|read_ref_at
 DECL|function|resolve_ref
@@ -19,3 +24,7 @@ DECL|function|unlock_ref
 DECL|function|verify_lock
 DECL|function|write_ref_sha1
 DECL|macro|MAXDEPTH
+DECL|member|name
+DECL|member|next
+DECL|member|sha1
+DECL|struct|ref_list
