51a8b95cbd333d2f32371e9ac493cc30bc0b49aa
Junio C Hamano <junkio@cox.net>
2006-09-12 22:42:31 -0700

    builtin-archive.c: rename remote_request() to extract_remote_arg()
    Suggested by Franck, and I think it makes sense.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-archive.c
+++ builtin-archive.c
@@ -1,10 +1,10 @@
 DECL|function|cmd_archive
 DECL|function|default_parse_extra
+DECL|function|extract_remote_arg
 DECL|function|init_archiver
 DECL|function|parse_archive_args
 DECL|function|parse_pathspec_arg
 DECL|function|parse_treeish_arg
-DECL|function|remote_request
 DECL|function|run_remote_archiver
 DECL|variable|archive_usage
 DECL|variable|archivers
