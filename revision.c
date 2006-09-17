28e6153ef2338101a7526df50076cd29c4f32e3e
Junio C Hamano <junkio@cox.net>
2006-09-17 15:43:40 -0700

    revision traversal: prepare for commit log match.
    This is from a suggestion by Linus, just to mark the locations where we
    need to modify to actually implement the filtering.
    
    We do not have any actual filtering code yet.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- revision.c
+++ revision.c
@@ -4,6 +4,7 @@ DECL|function|add_parents_only
 DECL|function|add_parents_to_list
 DECL|function|add_pending_commit_list
 DECL|function|add_pending_object
+DECL|function|commit_match
 DECL|function|everybody_uninteresting
 DECL|function|file_add_remove
 DECL|function|file_change
