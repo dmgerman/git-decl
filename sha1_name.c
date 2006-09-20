63a666363058b2e21a835de7797b3350d6ebc298
Junio C Hamano <junkio@cox.net>
2006-09-20 16:11:08 -0700

    sha1_name.c: understand "describe" output as a valid object name
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- sha1_name.c
+++ sha1_name.c
@@ -3,6 +3,7 @@ DECL|function|find_short_object_filename
 DECL|function|find_short_packed_object
 DECL|function|find_unique_abbrev
 DECL|function|find_unique_short_object
+DECL|function|get_describe_name
 DECL|function|get_nth_ancestor
 DECL|function|get_parent
 DECL|function|get_sha1
