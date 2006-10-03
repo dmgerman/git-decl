1d28800f5f4fc735b166ea7256d39f60772da77d
Junio C Hamano <junkio@cox.net>
2006-10-03 02:15:18 -0700

    pack-refs: use lockfile as everybody else does.
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-pack-refs.c
+++ builtin-pack-refs.c
@@ -3,7 +3,6 @@ DECL|function|do_not_prune
 DECL|function|handle_one_ref
 DECL|function|prune_ref
 DECL|function|prune_refs
-DECL|function|remove_lock_file
 DECL|member|name
 DECL|member|next
 DECL|member|prune
@@ -13,5 +12,4 @@ DECL|member|sha1
 DECL|struct|pack_refs_cb_data
 DECL|struct|ref_to_prune
 DECL|variable|builtin_pack_refs_usage
-DECL|variable|lock_path
-DECL|variable|result_path
+DECL|variable|packed
