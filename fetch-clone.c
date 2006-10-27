467a35c8b13903c993e0c2993caf194d91a503b5
Nicolas Pitre <nico@cam.org>
2006-10-27 15:42:17 -0400

    enhance clone and fetch -k experience
    Now that index-pack can be streamed with a pack, it is probably a good
    idea to use it directly instead of creating a temporary file and running
    index-pack afterwards.  This way index-pack can abort early whenever a
    corruption is encountered even if the pack has not been fully
    downloaded, it can display a progress percentage as it knows how much to
    expects, and it is a bit faster since the pack indexing is partially
    done as data is received. Using fetch -k doesn't need to disable thin
    pack generation on the remote end either.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- fetch-clone.c
+++ fetch-clone.c
@@ -1,6 +1,4 @@
-DECL|function|finish_pack
+DECL|function|get_pack
 DECL|function|receive_keep_pack
 DECL|function|receive_unpack_pack
 DECL|function|setup_sideband
-DECL|macro|NR_AVERAGE
-DECL|macro|usec_to_binarymsec
