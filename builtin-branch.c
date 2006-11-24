81e5bff712e92b19c0e3f07cc359c2a097edee25
Lars Hjemli <hjemli@gmail.com>
2006-11-24 14:45:10 +0100

    Add -v and --abbrev options to git-branch
    The new -v option makes git-branch show the abbreviated sha1 + subjectline
    for each branch.
    
    Additionally, minimum abbreviation length can be specified with
    --abbrev=<length>
    
    Signed-off-by: Lars Hjemli <hjemli@gmail.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-branch.c
+++ builtin-branch.c
@@ -4,6 +4,7 @@ DECL|function|create_branch
 DECL|function|delete_branches
 DECL|function|free_ref_list
 DECL|function|in_merge_bases
+DECL|function|print_ref_info
 DECL|function|print_ref_list
 DECL|function|ref_cmp
 DECL|macro|REF_LOCAL_BRANCH
@@ -15,7 +16,9 @@ DECL|member|index
 DECL|member|kind
 DECL|member|kinds
 DECL|member|list
+DECL|member|maxwidth
 DECL|member|name
+DECL|member|sha1
 DECL|struct|ref_item
 DECL|struct|ref_list
 DECL|variable|builtin_branch_usage
