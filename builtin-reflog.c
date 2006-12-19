d44dc7e82a4a423ce8a517edb707909d1acabf36
Junio C Hamano <junkio@cox.net>
2006-12-19 00:23:12 -0800

    git reflog expire
    This prepares a place to collect reflog management subcommands,
    and implements "expire" action.
    
    	$ git reflog expire --dry-run 		--expire=4.weeks 		--expire-unreachable=1.week 		refs/heads/master
    
    The expiration uses two timestamps: --expire and --expire-unreachable.
    Entries older than expire time (defaults to 90 days), and entries older
    than expire-unreachable time (defaults to 30 days) and records a commit
    that has been rewound and made unreachable from the current tip of the
    ref are removed from the reflog.
    
    The parameter handling is still rough, but I think the
    core logic for expiration is already sound.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

---
+++ builtin-reflog.c
@@ -0,0 +1,17 @@
+DECL|function|cmd_reflog
+DECL|function|cmd_reflog_expire
+DECL|function|expire_reflog
+DECL|function|expire_reflog_ent
+DECL|function|keep_entry
+DECL|member|dry_run
+DECL|member|expire_total
+DECL|member|expire_total
+DECL|member|expire_unreachable
+DECL|member|expire_unreachable
+DECL|member|newlog
+DECL|member|ref
+DECL|member|ref_commit
+DECL|struct|cmd_reflog_expire_cb
+DECL|struct|expire_reflog_cb
+DECL|variable|reflog_expire_usage
+DECL|variable|reflog_usage
