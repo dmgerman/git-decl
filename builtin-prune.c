ea42429ad7dda867e148244ef23f764968820de4
Matthias Lederhofer <matled@gmx.net>
2007-01-19 11:49:35 +0100

    prune: --grace=time
    This option gives grace period to objects that are unreachable
    from the refs from getting pruned.
    
    The default value is 24 hours and may be changed using
    gc.prunegrace.
    
    Signed-off-by: Matthias Lederhofer <matled@gmx.net>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-prune.c
+++ builtin-prune.c
@@ -1,6 +1,8 @@
 DECL|function|cmd_prune
+DECL|function|git_prune_config
 DECL|function|prune_dir
 DECL|function|prune_object
 DECL|function|prune_object_dir
+DECL|variable|prune_grace_period
 DECL|variable|prune_usage
 DECL|variable|show_only
