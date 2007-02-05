4e32581f6df28a4d92f0424f19d05cea88852476
Junio C Hamano <junkio@cox.net>
2007-02-04 17:50:14 -0800

    Rename get_ident() to fmt_ident() and make it available to outside
    This makes the functionality of ident.c::get_ident() available to
    other callers.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- ident.c
+++ ident.c
@@ -3,7 +3,7 @@ DECL|function|copy
 DECL|function|copy_email
 DECL|function|copy_gecos
 DECL|function|crud
-DECL|function|get_ident
+DECL|function|fmt_ident
 DECL|function|git_author_info
 DECL|function|git_committer_info
 DECL|function|setup_ident
