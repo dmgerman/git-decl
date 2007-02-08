f6bd208896854fa8993f11036dca80d7db3d5e69
Linus Torvalds <torvalds@linux-foundation.org>
2007-02-08 09:51:56 -0800

    git reflog show
    It makes "git reflog [show]" act as
    
    	git log -g --pretty=oneline --abbrev-cmit
    
    and is fairly straightforward. So you can just write
    
    	git reflog
    
    or
    
    	git reflog show
    
    and it will show you the reflog in a nice format.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-log.c
+++ builtin-log.c
@@ -3,6 +3,7 @@ DECL|function|cmd_cherry
 DECL|function|cmd_format_patch
 DECL|function|cmd_log
 DECL|function|cmd_log_init
+DECL|function|cmd_log_reflog
 DECL|function|cmd_log_walk
 DECL|function|cmd_show
 DECL|function|cmd_whatchanged
