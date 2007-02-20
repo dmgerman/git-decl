108a90a6c078eae7a8b8f3260cbe8145d3e7f819
Jason Riedy <ejr@EECS.Berkeley.EDU>
2007-02-19 16:27:09 -0800

    Obey NO_C99_FORMAT in fast-import.c.
    Define UM_FMT and UM10_FMT and use in place of %ju and %10ju,
    respectively.  Both format as unsigned long long, so this
    assumes the compiler supports long long.
    
    Signed-off-by: Jason Riedy <jason@acm.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- fast-import.c
+++ fast-import.c
@@ -66,6 +66,10 @@ DECL|function|update_branch
 DECL|function|validate_raw_date
 DECL|macro|MAX_PACK_ID
 DECL|macro|PACK_ID_BITS
+DECL|macro|UM10_FMT
+DECL|macro|UM10_FMT
+DECL|macro|UM_FMT
+DECL|macro|UM_FMT
 DECL|member|active_next_branch
 DECL|member|branch_tree
 DECL|member|buffer
