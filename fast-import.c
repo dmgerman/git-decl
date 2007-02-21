f29dcdef8e35cba2b625b6f36c144b5410c6fe93
Jason Riedy <ejr@EECS.Berkeley.EDU>
2007-02-20 17:34:56 -0800

    Check for PRIuMAX rather than NO_C99_FORMAT in fast-import.c.
    Thanks to Simon 'corecode' Schubert <corecode@fs.ei.tum.de> for
    the clean-up.  Defining the C99 standard PRIuMAX when necessary
    replaces UM_FMT and the awkward UM10_FMT.  There are no direct
    C99 translations for other uses of NO_C99_FORMAT in git, alas.
    
    Signed-off-by: Jason Riedy <ejr@cs.berkeley.edu>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- fast-import.c
+++ fast-import.c
@@ -66,10 +66,7 @@ DECL|function|update_branch
 DECL|function|validate_raw_date
 DECL|macro|MAX_PACK_ID
 DECL|macro|PACK_ID_BITS
-DECL|macro|UM10_FMT
-DECL|macro|UM10_FMT
-DECL|macro|UM_FMT
-DECL|macro|UM_FMT
+DECL|macro|PRIuMAX
 DECL|member|active_next_branch
 DECL|member|branch_tree
 DECL|member|buffer
