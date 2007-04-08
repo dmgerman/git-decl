b0862efefd1927d87e19fc43ab2dc58be056e630
Junio C Hamano <junkio@cox.net>
2007-04-08 01:28:00 -0700

    shortlog -w: make wrap-line behaviour optional.
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-shortlog.c
+++ builtin-shortlog.c
@@ -3,8 +3,14 @@ DECL|function|compare_by_number
 DECL|function|get_from_rev
 DECL|function|insert_author_oneline
 DECL|function|map_email
+DECL|function|parse_uint
+DECL|function|parse_wrap_args
 DECL|function|read_from_stdin
 DECL|function|read_mailmap
+DECL|macro|DEFAULT_INDENT1
+DECL|macro|DEFAULT_INDENT2
+DECL|macro|DEFAULT_WRAPLEN
 DECL|variable|common_repo_prefix
 DECL|variable|mailmap
 DECL|variable|shortlog_usage
+DECL|variable|wrap_arg_usage
