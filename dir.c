3d17361908d832e7593b6ed1ef1117fdeda0c0e1
Linus Torvalds <torvalds@linux-foundation.org>
2007-04-11 14:49:44 -0700

    Teach directory traversal about subprojects
    This is the promised cleaned-up version of teaching directory traversal
    (ie the "read_directory()" logic) about subprojects. That makes "git add"
    understand to add/update subprojects.
    
    It now knows to look at the index file to see if a directory is marked as
    a subproject, and use that as information as whether it should be recursed
    into or not.
    
    It also generally cleans up the handling of directory entries when
    traversing the working tree, by splitting up the decision-making process
    into small functions of their own, and adding a fair number of comments.
    
    Finally, it teaches "add_file_to_cache()" that directory names can have
    slashes at the end, since the directory traversal adds them to make the
    difference between a file and a directory clear (it always did that, but
    my previous too-ugly-to-apply subproject patch had a totally different
    path for subproject directories and avoided the slash for that case).
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- dir.c
+++ dir.c
@@ -1,3 +1,11 @@
+DECL|enumerator|ignore_directory
+DECL|enumerator|index_directory
+DECL|enumerator|index_gitdir
+DECL|enumerator|index_nonexistent
+DECL|enumerator|recurse_into_directory
+DECL|enumerator|show_directory
+DECL|enum|directory_treatment
+DECL|enum|exist_status
 DECL|function|add_exclude
 DECL|function|add_excludes_from_file
 DECL|function|add_excludes_from_file_1
@@ -5,7 +13,7 @@ DECL|function|cmp_name
 DECL|function|common_prefix
 DECL|function|create_simplify
 DECL|function|dir_add_name
-DECL|function|dir_exists
+DECL|function|directory_exists_in_index
 DECL|function|excluded
 DECL|function|excluded_1
 DECL|function|file_exists
@@ -18,6 +26,7 @@ DECL|function|read_directory
 DECL|function|read_directory_recursive
 DECL|function|simple_length
 DECL|function|simplify_away
+DECL|function|treat_directory
 DECL|member|len
 DECL|member|path
 DECL|struct|path_simplify
