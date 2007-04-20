a7870ac3b72343f6cc150d9977e49af9686aca7f
Nicolas Pitre <nico@cam.org>
2007-04-20 15:05:27 -0400

    provide a facility for "delayed" progress reporting
    This allows for progress to be displayed only if the progress has not
    reached a specified percentage treshold within a given delay in seconds.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- progress.c
+++ progress.c
@@ -3,5 +3,6 @@ DECL|function|display_progress
 DECL|function|progress_interval
 DECL|function|set_progress_signal
 DECL|function|start_progress
+DECL|function|start_progress_delay
 DECL|function|stop_progress
 DECL|variable|progress_update
--- progress.h
+++ progress.h
@@ -1,4 +1,7 @@
 DECL|macro|__progress_h__
+DECL|member|delay
+DECL|member|delayed_percent_treshold
+DECL|member|delayed_title
 DECL|member|last_percent
 DECL|member|prefix
 DECL|member|total
