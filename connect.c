618a22842a3de88f3917883081a0e3c646e9ba72
Alex Riesen <raa.lkml@gmail.com>
2007-05-23 23:34:27 +0200

    Verbose connect messages to show the IP addresses used
    Also, the patch makes the error messages more verbose. Helps when
    diagnosing connect problems on weird systems.
    
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- connect.c
+++ connect.c
@@ -2,6 +2,7 @@ DECL|enumerator|PROTO_GIT
 DECL|enumerator|PROTO_LOCAL
 DECL|enumerator|PROTO_SSH
 DECL|enum|protocol
+DECL|function|ai_name
 DECL|function|check_ref
 DECL|function|count_refspec_match
 DECL|function|find_ref_by_name
