38d2c2125d295cee05c6d766ae13a83efe02e05e
Alex Riesen <raa.lkml@gmail.com>
2007-05-23 18:07:25 +0200

    Fix the progress code to output LF only when it is really needed
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- progress.h
+++ progress.h
@@ -3,6 +3,7 @@ DECL|member|delay
 DECL|member|delayed_percent_treshold
 DECL|member|delayed_title
 DECL|member|last_percent
+DECL|member|need_lf
 DECL|member|prefix
 DECL|member|total
 DECL|struct|progress
