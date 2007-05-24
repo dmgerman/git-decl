fb013eed69dd5088edd8d1ae44a6a3174fe70b87
Fernando J. Pereda <ferdy@gentoo.org>
2007-05-25 00:15:36 +0200

    Teach mailsplit about Maildir's
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-mailsplit.c
+++ builtin-mailsplit.c
@@ -1,5 +1,7 @@
 DECL|function|cmd_mailsplit
 DECL|function|is_from_line
+DECL|function|populate_maildir_list
+DECL|function|split_maildir
 DECL|function|split_mbox
 DECL|function|split_one
 DECL|variable|buf
