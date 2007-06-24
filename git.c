93a4f59ec678d279c61cce8e8f2ffeaf8d053f7e
Linus Torvalds <torvalds@linux-foundation.org>
2007-06-24 10:10:40 -0700

    Clean up internal command handling
    This should change no code at all, it just moves the definition of "struct
    cmd_struct" out, and then splits out the running of the right command into
    the "run_command()" function.
    
    It also removes the long-unused 'envp' pointer passing.
    
    This is just preparation for adding some more error checking.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git.c
+++ git.c
@@ -4,10 +4,15 @@ DECL|function|handle_internal_command
 DECL|function|handle_options
 DECL|function|main
 DECL|function|prepend_to_path
+DECL|function|run_command
 DECL|function|split_cmdline
 DECL|macro|NOT_BARE
 DECL|macro|RUN_SETUP
 DECL|macro|USE_PAGER
+DECL|member|cmd
+DECL|member|fn
+DECL|member|option
+DECL|struct|cmd_struct
 DECL|variable|alias_command
 DECL|variable|alias_string
 DECL|variable|git_usage_string
