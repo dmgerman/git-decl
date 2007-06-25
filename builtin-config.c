c4d4ef584963249e62e9ed823518005fb747fbab
Frank Lichtenheld <frank@lichtenheld.de>
2007-06-25 16:03:55 +0200

    config: Add --null/-z option for null-delimted output
    Use \n as delimiter between key and value and \0 as
    delimiter after each key/value pair. This should be
    easily parsable output.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-config.c
+++ builtin-config.c
@@ -5,13 +5,16 @@ DECL|function|cmd_config
 DECL|function|get_value
 DECL|function|show_all_config
 DECL|function|show_config
+DECL|variable|delim
 DECL|variable|do_all
 DECL|variable|do_not_match
 DECL|variable|git_config_set_usage
 DECL|variable|key
+DECL|variable|key_delim
 DECL|variable|key_regexp
 DECL|variable|regexp
 DECL|variable|seen
 DECL|variable|show_keys
+DECL|variable|term
 DECL|variable|type
 DECL|variable|use_key_regexp
