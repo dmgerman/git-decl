ddf2d2360bf5eea0e73db481d975fba211eed63c
Linus Torvalds <torvalds@linux-foundation.org>
2007-08-09 22:21:29 -0700

    Start moving unpack-trees to "struct tree_desc"
    This doesn't actually change any real code, but it changes the interface
    to unpack_trees() to take an array of "struct tree_desc" entries, the same
    way the tree-walk.c functions do.
    
    The reason for this is that we would be much better off if we can do the
    tree-unpacking using the generic "traverse_trees()" functionality instead
    of having to the special "unpack" infrastructure.
    
    This really is a pretty minimal diff, just to change the calling
    convention. It passes all the tests, and looks sane. There were only two
    users of "unpack_trees()": builtin-read-tree and merge-recursive, and I
    tried to keep the changes minimal.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-read-tree.c
+++ builtin-read-tree.c
@@ -4,5 +4,6 @@ DECL|function|prime_cache_tree
 DECL|function|prime_cache_tree_rec
 DECL|function|read_cache_unmerged
 DECL|variable|lock_file
+DECL|variable|nr_trees
 DECL|variable|read_tree_usage
 DECL|variable|trees
