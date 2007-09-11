3e71c3c4c854099a1690f4aaa09da74ef5c867a6
Daniel Barkalow <barkalow@iabervon.org>
2007-09-10 23:02:34 -0400

    Make function to refill http queue a callback
    This eliminates the last function provided by the code using http.h as
    a global symbol, so it should be possible to have multiple programs
    using http.h in the same executable, and it also adds an argument to
    that callback, so that info can be passed into the callback without
    being global.
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http.c
+++ http.c
@@ -1,3 +1,4 @@
+DECL|function|add_fill_function
 DECL|function|closedown_active_slot
 DECL|function|fill_active_slots
 DECL|function|finish_active_slot
@@ -15,6 +16,10 @@ DECL|function|release_active_slot
 DECL|function|run_active_slot
 DECL|function|start_active_slot
 DECL|function|step_active_slots
+DECL|member|data
+DECL|member|fill
+DECL|member|next
+DECL|struct|fill_chain
 DECL|variable|active_queue_head
 DECL|variable|active_requests
 DECL|variable|curl_default
@@ -25,6 +30,7 @@ DECL|variable|curl_low_speed_time
 DECL|variable|curl_ssl_verify
 DECL|variable|curlm
 DECL|variable|data_received
+DECL|variable|fill_cfg
 DECL|variable|max_requests
 DECL|variable|pragma_header
 DECL|variable|ssl_cainfo
