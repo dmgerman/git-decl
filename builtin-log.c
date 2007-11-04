ed581689d0daf627c19a42be89105dd74aa1aec3
Brian Gernhardt <benji@silverinsanity.com>
2007-11-03 23:38:24 -0400

    format-patch: Add configuration and off switch for --numbered
    format.numbered is a tri-state variable.  Boolean values enable or
    disable numbering by default and "auto" enables number when outputting
    more than one patch.
    
    --no-numbered (short: -N) will disable numbering.
    
    Signed-off-by: Brian Gernhardt <benji@silverinsanity.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-log.c
+++ builtin-log.c
@@ -19,11 +19,13 @@ DECL|function|reopen_stdout
 DECL|function|show_object
 DECL|function|show_tree_object
 DECL|macro|FORMAT_PATCH_NAME_MAX
+DECL|variable|auto_number
 DECL|variable|cherry_usage
 DECL|variable|default_show_root
 DECL|variable|extra_headers
 DECL|variable|extra_headers_size
 DECL|variable|fmt_patch_subject_prefix
 DECL|variable|fmt_patch_suffix
+DECL|variable|numbered
 DECL|variable|output_directory
 DECL|variable|realstdout
