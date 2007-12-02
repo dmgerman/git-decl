21f5554bf3063780aabb582cafb06558766f0eb3
Christian Couder <chriscool@tuxfamily.org>
2007-12-02 06:07:40 +0100

    git-help: add -i|--info option to display info page.
    "git help --info subcommand" will now call "info git-subcommand".
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- help.c
+++ help.c
@@ -1,5 +1,7 @@
 DECL|function|add_cmdname
+DECL|function|check_help_cmd
 DECL|function|cmd_help
+DECL|function|cmd_to_page
 DECL|function|cmd_version
 DECL|function|cmdname_compare
 DECL|function|exclude_cmds
@@ -9,6 +11,7 @@ DECL|function|list_commands_in_dir
 DECL|function|list_common_cmds_help
 DECL|function|mput_char
 DECL|function|pretty_print_string_list
+DECL|function|show_info_page
 DECL|function|show_man_page
 DECL|function|term_columns
 DECL|function|uniq
