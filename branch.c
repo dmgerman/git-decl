eb16d82a31cb42762b49a85852b3760c6548e307
Daniel Barkalow <barkalow@iabervon.org>
2008-02-07 11:40:08 -0500

    Move create_branch into a library file
    You can also create branches, in exactly the same way, with checkout -b.
    
    This introduces branch.{c,h} library files for doing porcelain-level
    operations on branches (such as creating them with their appropriate
    default configuration).
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>

---
+++ branch.c
@@ -0,0 +1,8 @@
+DECL|function|create_branch
+DECL|function|find_tracked_branch
+DECL|function|setup_tracking
+DECL|member|matches
+DECL|member|remote
+DECL|member|spec
+DECL|member|src
+DECL|struct|tracking
