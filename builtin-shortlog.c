99a3e956706403643ba5da0fc7b542abba9e9f82
Daniel Barkalow <barkalow@iabervon.org>
2008-02-25 18:24:14 -0500

    Add API access to shortlog
    Shortlog is gives a pretty simple API for cases where you're already
    identifying all of the individual commits. Make this available to
    other code instead of requiring them to use the revision API and
    command line.
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>

--- builtin-shortlog.c
+++ builtin-shortlog.c
@@ -5,11 +5,11 @@ DECL|function|insert_one_record
 DECL|function|parse_uint
 DECL|function|parse_wrap_args
 DECL|function|read_from_stdin
+DECL|function|shortlog_add_commit
+DECL|function|shortlog_init
+DECL|function|shortlog_output
 DECL|macro|DEFAULT_INDENT1
 DECL|macro|DEFAULT_INDENT2
 DECL|macro|DEFAULT_WRAPLEN
-DECL|variable|common_repo_prefix
-DECL|variable|email
-DECL|variable|mailmap
 DECL|variable|shortlog_usage
 DECL|variable|wrap_arg_usage
