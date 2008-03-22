dd7f974fcd8d171cad167b1e651755c5503cb5aa
Linus Torvalds <torvalds@woody.linux-foundation.org>
2008-03-22 14:22:44 -0700

    Make git-add behave more sensibly in a case-insensitive environment
    This expands on the previous patch, and allows "git add" to sanely handle
    a filename that has changed case, keeping the case in the index constant,
    and avoiding aliases.
    
    In particular, if you have an index entry called "File", but the
    checked-out tree is case-corrupted and has an entry called "file"
    instead, doing a
    
    	git add .
    
    (or naming "file" explicitly) will automatically notice that we have an
    alias, and will replace the name "file" with the existing index
    capitalization (ie "File").
    
    However, if we actually have *both* a file called "File" and one called
    "file", and they don't have the same lstat() information (ie we're on a
    case-sensitive filesystem but have the "core.ignorecase" flag set), we
    will error out if we try to add them both.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- read-cache.c
+++ read-cache.c
@@ -18,7 +18,9 @@ DECL|function|ce_write_flush
 DECL|function|check_file_directory_conflict
 DECL|function|compare_name
 DECL|function|convert_from_disk
+DECL|function|create_alias_ce
 DECL|function|df_name_compare
+DECL|function|different_name
 DECL|function|discard_index
 DECL|function|estimate_cache_size
 DECL|function|fill_stat_cache_info
