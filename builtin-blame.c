3b985de465ff4c0ee6ea1fd59beb51e106614cda
Junio C Hamano <gitster@pobox.com>
2008-04-02 22:17:53 -0700

    builtin-blame.c: move prepare_final() into a separate function.
    After parsing the command line, we have a long loop to compute the commit
    object to start annotating from.  Move the logic to a separate function,
    so that later patches become easier to read.
    
    It also makes fill_origin_blob() return void; the check is always done
    on !file->ptr, and nobody looks at the return value from the function.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-blame.c
+++ builtin-blame.c
@@ -45,6 +45,7 @@ DECL|function|pass_blame
 DECL|function|pass_blame_to_parent
 DECL|function|pass_whole_blame
 DECL|function|prepare_blame_range
+DECL|function|prepare_final
 DECL|function|prepare_lines
 DECL|function|process_u_diff
 DECL|function|read_ancestry
