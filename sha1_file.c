a720929d5bd325ba5f871802236ba997c41fe370
Daniel Barkalow <barkalow@iabervon.org>
2008-04-17 19:32:30 -0400

    Add a library function to add an alternate to the alternates file
    This is in the core so that, if the alternates file has already been
    read, the addition can be parsed and put into effect for the current
    process.
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -1,5 +1,6 @@
 DECL|function|add_delta_base_cache
 DECL|function|add_packed_git
+DECL|function|add_to_alternates_file
 DECL|function|cache_or_unpack_entry
 DECL|function|check_packed_git_idx
 DECL|function|check_sha1_signature
