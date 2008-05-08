53ccc9670fb878c61ca0960efee616df34c9f496
Johannes Sixt <j.sixt@viscovery.net>
2008-05-08 09:34:49 +0200

    compat-util: avoid macro redefinition warning
    Some systems define fopen as a macro based on compiler settings, and
    unconditionally redefining it triggers a compilation warning.

--- git-compat-util.h
+++ git-compat-util.h
@@ -56,6 +56,7 @@ DECL|macro|_XOPEN_SOURCE_EXTENDED
 DECL|macro|__attribute__
 DECL|macro|decimal_length
 DECL|macro|fopen
+DECL|macro|fopen
 DECL|macro|hstrerror
 DECL|macro|isalnum
 DECL|macro|isalnum
