bff907788826c86005e3b9574436bafc056c28c5
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2008-06-27 13:24:47 +0100

    commit-tree: lift completely arbitrary limit of 16 parents
    There is no really good reason to have a merge with more than 16
    parents, but we have a history of giving our users rope.
    
    Combined with the fact that there was no good reason for that
    arbitrary limit in the first place, here is an all-too-easy to fix.
    
    Kind of wished-for by Len Brown.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-commit-tree.c
+++ builtin-commit-tree.c
@@ -2,7 +2,5 @@ DECL|function|check_valid
 DECL|function|cmd_commit_tree
 DECL|function|new_parent
 DECL|macro|BLOCKING
-DECL|macro|MAXPARENT
 DECL|variable|commit_tree_usage
 DECL|variable|commit_utf8_warn
-DECL|variable|parent_sha1
