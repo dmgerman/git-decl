090a4db556ccffc79f7f9b4c20cb42b3aa4339a4
Steffen Prohaska <prohaska@zib.de>
2008-07-13 22:31:18 +0200

    Move code interpreting path relative to exec-dir to new function system_path()
    Expanding system paths relative to git_exec_path can be used for
    creating an installation that can be moved to a different directory
    without re-compiling.  We use this approach for template_dir and the
    system wide gitconfig.  The Windows installer (msysgit) is an example
    for such a setup.
    
    This commit moves common code to a new function system_path().  System
    paths that are to be interpreted relative to git_exec_path are passed to
    system_path() and the return value is used instead of the original path.
    system_path() prefixes a relative path with git_exec_path and leaves
    absolute paths unmodified.  For example, we now write
    
        template_dir = system_path(DEFAULT_GIT_TEMPLATE_DIR);
    
    [j6t: moved from path.c to exec_cmd.c]
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Johannes Sixt <johannes.sixt@telecom.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- exec_cmd.c
+++ exec_cmd.c
@@ -5,5 +5,6 @@ DECL|function|execv_git_cmd
 DECL|function|git_exec_path
 DECL|function|git_set_argv_exec_path
 DECL|function|setup_path
+DECL|function|system_path
 DECL|macro|MAX_ARGS
 DECL|variable|argv_exec_path
