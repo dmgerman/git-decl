ae13fd79f6f12f273d5cf3b30734ec94624844a0
Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
2008-07-25 12:41:21 +0200

    archive: add write_archive()
    Both archive and upload-archive have to parse command line arguments and
    then call the archiver specific write function.  Move the duplicate code
    to a new function, write_archive().
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- archive.c
+++ archive.c
@@ -1,6 +1,7 @@
 DECL|function|format_subst
 DECL|function|setup_archive_check
 DECL|function|sha1_file_to_archive
+DECL|function|write_archive
 DECL|function|write_archive_entries
 DECL|function|write_archive_entry
 DECL|member|args
