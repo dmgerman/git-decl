7161b3884974c03c9820e4599de301980bc18231
Karl Hasselström <kha@treskal.com>
2008-08-08 22:48:23 +0200

    Refactoring: Split up diff_tree_stdin
    Into a first half that determines what operation to do, and a second
    half that does it.
    
    Currently the only operation is diffing one or more commits, but a
    later patch will add diffing of trees, at which point this refactoring
    will pay off.
    
    Signed-off-by: Karl Hasselström <kha@treskal.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-diff-tree.c
+++ builtin-diff-tree.c
@@ -1,5 +1,6 @@
 DECL|function|cmd_diff_tree
 DECL|function|diff_tree_commit_sha1
 DECL|function|diff_tree_stdin
+DECL|function|stdin_diff_commit
 DECL|variable|diff_tree_usage
 DECL|variable|log_tree_opt
