9551ec02e544e32debe3b184807e5c199a38f9e6
Junio C Hamano <gitster@pobox.com>
2008-08-29 10:49:56 -0700

    merge.conflictstyle: choose between "merge" and "diff3 -m" styles
    This teaches "git merge-file" to honor merge.conflictstyle configuration
    variable, whose value can be "merge" (default) or "diff3".
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- xdiff-interface.c
+++ xdiff-interface.c
@@ -1,6 +1,7 @@
 DECL|function|buffer_is_binary
 DECL|function|consume_one
 DECL|function|ff_regexp
+DECL|function|git_xmerge_config
 DECL|function|parse_hunk_header
 DECL|function|parse_num
 DECL|function|read_mmfile
@@ -15,3 +16,4 @@ DECL|member|nr
 DECL|member|re
 DECL|struct|ff_reg
 DECL|struct|ff_regs
+DECL|variable|git_xmerge_style
