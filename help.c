8bfce673a93c0d219e73e101f850edde9211437e
Alex Riesen <raa.lkml@gmail.com>
2008-08-31 15:54:58 +0200

    Add help.autocorrect to enable/disable autocorrecting
    It is off(0) by default, to avoid scaring people unless they asked to.
    If set to a non-0 value, wait for that amount of deciseconds before
    running the corrected command.
    
    Suggested by Junio, so he has a chance to hit Ctrl-C.
    
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- help.c
+++ help.c
@@ -3,6 +3,7 @@ DECL|function|clean_cmdnames
 DECL|function|cmd_version
 DECL|function|cmdname_compare
 DECL|function|exclude_cmds
+DECL|function|git_unknown_cmd_config
 DECL|function|help_unknown_cmd
 DECL|function|is_executable
 DECL|function|is_in_cmdlist
@@ -13,3 +14,4 @@ DECL|function|load_command_list
 DECL|function|pretty_print_string_list
 DECL|function|term_columns
 DECL|function|uniq
+DECL|variable|autocorrect
