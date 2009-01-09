9f42c72376c373aff530d7b6ba85173c72b1554d
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2009-01-10 00:18:34 +0100

    grep: don't call regexec() for fixed strings
    Add the new flag "fixed" to struct grep_pat and set it if the pattern
    is doesn't contain any regex control characters in addition to if the
    flag -F/--fixed-strings was specified.
    
    This gives a nice speed up on msysgit, where regexec() seems to be
    extra slow.  Before (best of five runs):
    
    	$ time git grep grep v1.6.1 >/dev/null
    
    	real    0m0.552s
    	user    0m0.000s
    	sys     0m0.000s
    
    	$ time git grep -F grep v1.6.1 >/dev/null
    
    	real    0m0.170s
    	user    0m0.000s
    	sys     0m0.015s
    
    With the patch:
    
    	$ time git grep grep v1.6.1 >/dev/null
    
    	real    0m0.173s
    	user    0m0.000s
    	sys     0m0.000s
    
    The difference is much smaller on Linux, but still measurable.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- grep.c
+++ grep.c
@@ -15,6 +15,8 @@ DECL|function|free_grep_patterns
 DECL|function|free_pattern_expr
 DECL|function|grep_buffer
 DECL|function|grep_buffer_1
+DECL|function|is_fixed
+DECL|function|isregexspecial
 DECL|function|match_expr
 DECL|function|match_expr_eval
 DECL|function|match_line
