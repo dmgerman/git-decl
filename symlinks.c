896b916d4f3a23f0bed1dc0b1117f2e6542812af
Kjetil Barvik <barvik@broadpark.no>
2009-01-18 16:14:51 +0100

    lstat_cache(): introduce has_symlink_or_noent_leading_path() function
    In some cases, especially inside the unpack-trees.c file, and inside
    the verify_absent() function, we can avoid some unnecessary calls to
    lstat(), if the lstat_cache() function can also be told to keep track
    of non-existing directories.
    
    So we update the lstat_cache() function to handle this new fact,
    introduce a new wrapper function, and the result is that we save lots
    of lstat() calls for a removed directory which previously contained
    lots of files, when we call this new wrapper of lstat_cache() instead
    of the old one.
    
    We do similar changes inside the unlink_entry() function, since if we
    can already say that the leading directory component of a pathname
    does not exist, it is not necessary to try to remove a pathname below
    it!
    
    Thanks to Junio C Hamano, Linus Torvalds and Rene Scharfe for valuable
    comments to this patch!
    
    Signed-off-by: Kjetil Barvik <barvik@broadpark.no>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- symlinks.c
+++ symlinks.c
@@ -1,13 +1,16 @@
 DECL|function|has_symlink_leading_path
+DECL|function|has_symlink_or_noent_leading_path
 DECL|function|longest_match_lstat_cache
 DECL|function|lstat_cache
 DECL|function|reset_lstat_cache
 DECL|macro|FL_DIR
 DECL|macro|FL_ERR
 DECL|macro|FL_LSTATERR
+DECL|macro|FL_NOENT
 DECL|macro|FL_SYMLINK
 DECL|member|flags
 DECL|member|len
 DECL|member|path
+DECL|member|track_flags
 DECL|struct|cache_def
 DECL|variable|cache
