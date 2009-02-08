87a795a08aedbcc28a952ea7379cf7bf6410fc53
Marius Storm-Olsen <marius@trolltech.com>
2009-02-08 15:34:28 +0100

    Add find_insert_index, insert_at_index and clear_func functions to string_list
    string_list_find_insert_index() and string_list_insert_at_index()
    enables you to see if an item is in the string_list, and to
    insert at the appropriate index in the list, if not there.
    This is usefull if you need to manipulate an existing item,
    if present, and insert a new item if not.
    
    Future mailmap code will use this construct to enable
    complex (old_name, old_email) -> (new_name, new_email)
    lookups.
    
    The string_list_clear_func() allows to call a custom
    cleanup function on each item in a string_list, which is
    useful is the util member points to a complex structure.
    
    Signed-off-by: Marius Storm-Olsen <marius@trolltech.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- string-list.c
+++ string-list.c
@@ -5,7 +5,10 @@ DECL|function|print_string_list
 DECL|function|sort_string_list
 DECL|function|string_list_append
 DECL|function|string_list_clear
+DECL|function|string_list_clear_func
+DECL|function|string_list_find_insert_index
 DECL|function|string_list_has_string
 DECL|function|string_list_insert
+DECL|function|string_list_insert_at_index
 DECL|function|string_list_lookup
 DECL|function|unsorted_string_list_has_string
