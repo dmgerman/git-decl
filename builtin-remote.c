5539fb16ffc4cd76fadbb12ac1f16a480c3bb6fe
Jay Soffian <jaysoffian@gmail.com>
2009-02-25 03:32:27 -0500

    builtin-remote: new show output style
    The existing output of "git remote show <remote>" is too verbose for the
    information it provides. This patch teaches it to provide more
    information in less space.
    
    The output for push refspecs is addressed in the next patch.
    
    Before the patch:
    
    $ git remote show origin
    * remote origin
      URL: git://git.kernel.org/pub/scm/git/git.git
      HEAD branch: master
      Remote branch merged with 'git pull' while on branch master
        master
      Remote branch merged with 'git pull' while on branch next
        next
      Remote branches merged with 'git pull' while on branch octopus
        foo bar baz frotz
      New remote branch (next fetch will store in remotes/origin)
        html
      Stale tracking branch (use 'git remote prune')
        bogus
      Tracked remote branches
        maint
        man
        master
        next
        pu
        todo
    
    After this patch:
    
    $ git remote show origin
    * remote origin
      URL: git://git.kernel.org/pub/scm/git/git.git
      HEAD branch: master
      Remote branches:
        bogus  stale (use 'git remote prune' to remove)
        html   new (next fetch will store in remotes/origin)
        maint  tracked
        man    tracked
        master tracked
        next   tracked
        pu     tracked
        todo   tracked
      Local branches configured for 'git pull':
        master  rebases onto remote master
        next    rebases onto remote next
        octopus  merges with remote foo
                    and with remote bar
                    and with remote baz
                    and with remote frotz
    
    $ git remote show origin -n
    * remote origin
      URL: git://git.kernel.org/pub/scm/git/git.git
      HEAD branch: (not queried)
      Remote branches: (status not queried)
        bogus
        maint
        man
        master
        next
        pu
        todo
      Local branches configured for 'git pull':
        master  rebases onto remote master
        next    rebases onto remote next
        octopus  merges with remote foo
                    and with remote bar
                    and with remote baz
                    and with remote frotz
    
    Signed-off-by: Jay Soffian <jaysoffian@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-remote.c
+++ builtin-remote.c
@@ -2,6 +2,8 @@ DECL|function|abbrev_ref
 DECL|function|add
 DECL|function|add_branch_for_removal
 DECL|function|add_known_remote
+DECL|function|add_local_to_show_info
+DECL|function|add_remote_to_show_info
 DECL|function|append_ref_to_tracked_list
 DECL|function|cmd_remote
 DECL|function|config_read_branches
@@ -26,14 +28,17 @@ DECL|function|rm
 DECL|function|set_head
 DECL|function|show
 DECL|function|show_all
-DECL|function|show_list
+DECL|function|show_local_info_item
+DECL|function|show_remote_info_item
 DECL|function|update
 DECL|macro|GET_HEAD_NAMES
 DECL|macro|GET_REF_STATES
 DECL|macro|abbrev_branch
+DECL|member|any_rebase
 DECL|member|branches
 DECL|member|heads
 DECL|member|keep
+DECL|member|list
 DECL|member|list
 DECL|member|list
 DECL|member|merge
@@ -42,6 +47,8 @@ DECL|member|new
 DECL|member|new
 DECL|member|next
 DECL|member|old
+DECL|member|queried
+DECL|member|rebase
 DECL|member|remote
 DECL|member|remote
 DECL|member|remote
@@ -49,8 +56,10 @@ DECL|member|remote_branches
 DECL|member|remote_name
 DECL|member|skipped
 DECL|member|stale
+DECL|member|states
 DECL|member|to_delete
 DECL|member|tracked
+DECL|member|width
 DECL|struct|branch_info
 DECL|struct|branches_for_remote
 DECL|struct|known_remote
@@ -58,6 +67,7 @@ DECL|struct|known_remotes
 DECL|struct|ref_states
 DECL|struct|remote_group
 DECL|struct|rename_info
+DECL|struct|show_info
 DECL|variable|branch_list
 DECL|variable|builtin_remote_usage
 DECL|variable|remote_group
