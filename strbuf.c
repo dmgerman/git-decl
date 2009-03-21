484cd87ec1c8cefd1429d5227d7b869680538218
Junio C Hamano <gitster@pobox.com>
2009-03-21 14:35:51 -0700

    strbuf_check_branch_ref(): a helper to check a refname for a branch
    This allows a common calling sequence
    
    	strbuf_branchname(&ref, name);
    	strbuf_splice(&ref, 0, 0, "refs/heads/", 11);
    	if (check_ref_format(ref.buf))
    		die(...);
    
    to be refactored into
    
    	if (strbuf_check_branch_ref(&ref, name))
    		die(...);
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- strbuf.c
+++ strbuf.c
@@ -4,6 +4,7 @@ DECL|function|strbuf_adddup
 DECL|function|strbuf_addf
 DECL|function|strbuf_attach
 DECL|function|strbuf_branchname
+DECL|function|strbuf_check_branch_ref
 DECL|function|strbuf_cmp
 DECL|function|strbuf_detach
 DECL|function|strbuf_expand
