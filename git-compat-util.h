777b5722430ab73214f42b757df6cff28788f295
Pierre Habouzit <madcoder@debian.org>
2009-07-22 23:34:34 +0200

    refactor: use bitsizeof() instead of 8 * sizeof()
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-compat-util.h
+++ git-compat-util.h
@@ -61,6 +61,7 @@ DECL|macro|_XOPEN_SOURCE
 DECL|macro|_XOPEN_SOURCE_EXTENDED
 DECL|macro|__attribute__
 DECL|macro|basename
+DECL|macro|bitsizeof
 DECL|macro|decimal_length
 DECL|macro|fopen
 DECL|macro|fopen
