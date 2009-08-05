3f9ac9741f07283583155415a5ebf742b3e25fe8
Junio C Hamano <gitster@pobox.com>
2009-08-04 22:08:16 -0700

    diff-index: keep the original index intact
    When comparing the index and a tree, we used to read the contents of the
    tree into stage #1 of the index and compared them with stage #0.  In order
    not to lose sight of entries originally unmerged in the index, we hoisted
    them to stage #3 before reading the tree.
    
    Commit d1f2d7e (Make run_diff_index() use unpack_trees(), not read_tree(),
    2008-01-19) changed all this.  These days, we instead use unpack_trees()
    API to traverse the tree and compare the contents with the index, without
    modifying the index at all.  There is no reason to hoist the unmerged
    entries to stage #3 anymore.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff-lib.c
+++ diff-lib.c
@@ -4,7 +4,6 @@ DECL|function|do_diff_cache
 DECL|function|do_oneway_diff
 DECL|function|get_stat_data
 DECL|function|index_differs_from
-DECL|function|mark_merge_entries
 DECL|function|oneway_diff
 DECL|function|run_diff_files
 DECL|function|run_diff_index
