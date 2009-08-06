e32331c80404d070a163ad969c1a708d04419606
Linus Torvalds <torvalds@linux-foundation.org>
2009-08-06 07:20:54 -0700

    block-sha1: macroize the rounds a bit further
    Avoid repeating the shared parts of the different rounds by adding a
    macro layer or two. It was already more cpp than C.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- block-sha1/sha1.c
+++ block-sha1/sha1.c
@@ -3,12 +3,14 @@ DECL|function|blk_SHA1_Final
 DECL|function|blk_SHA1_Init
 DECL|function|blk_SHA1_Update
 DECL|macro|SHA_ASM
+DECL|macro|SHA_MIX
 DECL|macro|SHA_ROL
 DECL|macro|SHA_ROL
 DECL|macro|SHA_ROR
 DECL|macro|SHA_ROR
 DECL|macro|SHA_ROT
-DECL|macro|SHA_XOR
+DECL|macro|SHA_ROUND
+DECL|macro|SHA_SRC
 DECL|macro|T_0_15
 DECL|macro|T_16_19
 DECL|macro|T_20_39
