ed5524ce73d10b73baaaa752867e983997881551
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2009-12-09 17:26:27 +0200

    Add remote helper debug mode
    Remote helpers deadlock easily, so support debug mode which shows the
    interaction steps.
    
    Signed-off-by: Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport-helper.c
+++ transport-helper.c
@@ -7,10 +7,14 @@ DECL|function|get_importer
 DECL|function|get_refs_list
 DECL|function|has_attribute
 DECL|function|push_refs
+DECL|function|recvline
 DECL|function|release_helper
+DECL|function|sendline
 DECL|function|set_helper_option
 DECL|function|standard_options
 DECL|function|transport_helper_init
+DECL|function|write_constant
+DECL|function|xchgline
 DECL|member|fetch
 DECL|member|helper
 DECL|member|import
@@ -22,4 +26,5 @@ DECL|member|refspec_nr
 DECL|member|refspecs
 DECL|struct|helper_data
 DECL|variable|boolean_options
+DECL|variable|debug
 DECL|variable|unsupported_options
