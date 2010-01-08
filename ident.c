d3e9bfba743263f057eb1ba9a89217fea5999429
Junio C Hamano <gitster@pobox.com>
2010-01-08 05:53:56 -0800

    ident.c: remove unused variables
    d5cc2de (ident.c: Trim hint printed when gecos is empty., 2006-11-28)
    reworded the message used as printf() format and dropped "%s" from it;
    these two variables that hold the names of GIT_{AUTHOR,COMMITTER}_NAME
    environment variables haven't been used since then.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ident.c
+++ ident.c
@@ -8,7 +8,5 @@ DECL|function|fmt_name
 DECL|function|git_author_info
 DECL|function|git_committer_info
 DECL|function|setup_ident
-DECL|variable|au_env
-DECL|variable|co_env
 DECL|variable|env_hint
 DECL|variable|git_default_date
