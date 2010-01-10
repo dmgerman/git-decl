5d16c692f06a12136b9aca60dba1720fae764c7a
Johannes Sixt <j6t@kdbg.org>
2010-01-10 14:11:22 +0100

    start_command: detect execvp failures early
    Previously, failures during execvp could be detected only by
    finish_command. However, in some situations it is beneficial for the
    parent process to know earlier that the child process will not run.
    
    The idea to use a pipe to signal failures to the parent process and
    the test case were lifted from patches by Ilari Liusvaara.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- run-command.c
+++ run-command.c
@@ -3,6 +3,7 @@ DECL|function|die_child
 DECL|function|dup_devnull
 DECL|function|finish_async
 DECL|function|finish_command
+DECL|function|notify_parent
 DECL|function|prepare_run_command_v_opt
 DECL|function|run_command
 DECL|function|run_command_v_opt
@@ -14,3 +15,4 @@ DECL|function|start_async
 DECL|function|start_command
 DECL|function|wait_or_whine
 DECL|variable|child_err
+DECL|variable|child_notifier
