9f0e81353e5d0972bfb2380d5649b251f310a8ef
Junio C Hamano <gitster@pobox.com>
2010-01-12 19:06:41 -0800

    grep: rip out support for external grep
    We still allow people to pass --[no-]ext-grep on the command line,
    but the option is ignored.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-grep.c
+++ builtin-grep.c
@@ -3,11 +3,7 @@ DECL|function|and_callback
 DECL|function|close_callback
 DECL|function|cmd_grep
 DECL|function|context_callback
-DECL|function|exec_grep
-DECL|function|external_grep
 DECL|function|file_callback
-DECL|function|flush_grep
-DECL|function|grep_add_color
 DECL|function|grep_cache
 DECL|function|grep_config
 DECL|function|grep_file
@@ -20,9 +16,4 @@ DECL|function|not_callback
 DECL|function|open_callback
 DECL|function|pathspec_matches
 DECL|function|pattern_callback
-DECL|macro|ARGBUF
-DECL|macro|MAXARGS
-DECL|macro|NO_EXTERNAL_GREP
-DECL|macro|NO_EXTERNAL_GREP
-DECL|macro|push_arg
 DECL|variable|grep_usage
