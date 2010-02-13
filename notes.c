f96eb85471f638d20b8775375fd5bbfa8f606014
Johan Herland <johan@herland.net>
2010-02-13 22:28:17 +0100

    Notes API: write_notes_tree(): Store the notes tree in the database
    Uses for_each_note() to traverse the notes tree, and produces tree
    objects on the fly representing the "on-disk" version of the notes
    tree with appropriate fanout.
    
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- notes.c
+++ notes.c
@@ -10,6 +10,7 @@ DECL|function|get_note
 DECL|function|get_sha1_hex_segment
 DECL|function|init_notes
 DECL|function|load_subtree
+DECL|function|matches_tree_write_stack
 DECL|function|note_tree_consolidate
 DECL|function|note_tree_find
 DECL|function|note_tree_free
@@ -17,6 +18,12 @@ DECL|function|note_tree_insert
 DECL|function|note_tree_remove
 DECL|function|note_tree_search
 DECL|function|remove_note
+DECL|function|tree_write_stack_finish_subtree
+DECL|function|tree_write_stack_init_subtree
+DECL|function|write_each_note
+DECL|function|write_each_note_helper
+DECL|function|write_notes_tree
+DECL|function|write_tree_entry
 DECL|macro|CLR_PTR_TYPE
 DECL|macro|GET_NIBBLE
 DECL|macro|GET_PTR_TYPE
@@ -27,9 +34,15 @@ DECL|macro|PTR_TYPE_SUBTREE
 DECL|macro|SET_PTR_TYPE
 DECL|macro|SUBTREE_SHA1_PREFIXCMP
 DECL|member|a
+DECL|member|buf
 DECL|member|key_sha1
+DECL|member|next
+DECL|member|path
+DECL|member|root
 DECL|member|val_sha1
 DECL|struct|int_node
 DECL|struct|leaf_node
+DECL|struct|tree_write_stack
+DECL|struct|write_each_note_data
 DECL|variable|initialized
 DECL|variable|root_node
