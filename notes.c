dc02e31a94c2a5f37303a768958b4a406b828fce
Johan Herland <johan@herland.net>
2010-02-13 22:28:14 +0100

    Notes API: remove_note(): Remove note objects from the notes tree structure
    This includes adding internal functions for maintaining a healthy notes tree
    structure after removing individual notes.
    
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- notes.c
+++ notes.c
@@ -6,10 +6,13 @@ DECL|function|get_sha1_hex_segment
 DECL|function|init_notes
 DECL|function|load_subtree
 DECL|function|lookup_notes
+DECL|function|note_tree_consolidate
 DECL|function|note_tree_find
 DECL|function|note_tree_free
 DECL|function|note_tree_insert
+DECL|function|note_tree_remove
 DECL|function|note_tree_search
+DECL|function|remove_note
 DECL|macro|CLR_PTR_TYPE
 DECL|macro|GET_NIBBLE
 DECL|macro|GET_PTR_TYPE
