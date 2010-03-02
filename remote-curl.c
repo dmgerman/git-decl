62c95ab67d0916e4a7bfc6ef8c7e357580c796b4
Tay Ray Chuan <rctay89@gmail.com>
2010-03-02 18:49:31 +0800

    remote-curl: init walker only when needed
    Invoke get_http_walker() only when fetching with the dumb protocol.
    Additionally, add an invocation to walker_free() after we're done using
    the walker.
    
    Signed-off-by: Tay Ray Chuan <rctay89@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote-curl.c
+++ remote-curl.c
@@ -4,7 +4,6 @@ DECL|function|fetch_dumb
 DECL|function|fetch_git
 DECL|function|free_discovery
 DECL|function|get_refs
-DECL|function|init_walker
 DECL|function|main
 DECL|function|output_refs
 DECL|function|parse_fetch
@@ -53,4 +52,3 @@ DECL|variable|last_discovery
 DECL|variable|options
 DECL|variable|remote
 DECL|variable|url
-DECL|variable|walker
