d10b26472c4c886c8812e871c735ff2b97247627
Johannes Sixt <j6t@kdbg.org>
2010-03-06 16:40:39 +0100

    Make report() from usage.c public as vreportf() and use it.
    There exist already a number of static functions named 'report', therefore,
    the function name was changed.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- usage.c
+++ usage.c
@@ -3,11 +3,11 @@ DECL|function|die_builtin
 DECL|function|die_errno
 DECL|function|error
 DECL|function|error_builtin
-DECL|function|report
 DECL|function|set_die_routine
 DECL|function|usage
 DECL|function|usage_builtin
 DECL|function|usagef
+DECL|function|vreportf
 DECL|function|warn_builtin
 DECL|function|warning
 DECL|variable|die_routine
