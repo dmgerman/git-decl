cafcf4bd7900e6c043cf3e0ac77f2b166842f08b
Jens Lehmann <Jens.Lehmann@web.de>
2010-03-11 22:50:25 +0100

    Refactor dirty submodule detection in diff-lib.c
    Moving duplicated code into the new function match_stat_with_submodule().
    Replacing the implicit activation of detailed checks for the dirtiness of
    submodules when DIFF_FORMAT_PATCH was selected with explicitly setting
    the recently added DIFF_OPT_DIRTY_SUBMODULES option in diff_setup_done().
    
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff-lib.c
+++ diff-lib.c
@@ -4,6 +4,7 @@ DECL|function|do_diff_cache
 DECL|function|do_oneway_diff
 DECL|function|get_stat_data
 DECL|function|index_differs_from
+DECL|function|match_stat_with_submodule
 DECL|function|oneway_diff
 DECL|function|run_diff_files
 DECL|function|run_diff_index
