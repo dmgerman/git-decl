9983881b6eddcdff1a16c839f21ef8bba4e7be75
Thomas Rast <trast@student.ethz.ch>
2010-03-12 18:04:32 +0100

    notes: implement helpers needed for note copying during rewrite
    Implement helper functions to load the rewriting config, and to
    actually copy the notes.  Also document the config.
    
    Secondly, also implement an undocumented --for-rewrite=<cmd> option to
    'git notes copy' which is used like --stdin, but also puts the
    configuration for <cmd> into effect.  It will be needed to support the
    copying in git-rebase.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Acked-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin.h
+++ builtin.h
@@ -1,1 +1,9 @@
 DECL|macro|BUILTIN_H
+DECL|member|cmd
+DECL|member|combine
+DECL|member|enabled
+DECL|member|mode_from_env
+DECL|member|refs
+DECL|member|refs_from_env
+DECL|member|trees
+DECL|struct|notes_rewrite_cfg
