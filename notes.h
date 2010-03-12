f846614b6588df1047443674071c4f872009bb67
Thomas Rast <trast@student.ethz.ch>
2010-03-12 18:04:26 +0100

    Support showing notes from more than one notes tree
    With this patch, you can set notes.displayRef to a glob that points at
    your favourite notes refs, e.g.,
    
    [notes]
    	displayRef = refs/notes/*
    
    Then git-log and friends will show notes from all trees.
    
    Thanks to Junio C Hamano for lots of feedback, which greatly
    influenced the design of the entire series and this commit in
    particular.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Acked-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- notes.h
+++ notes.h
@@ -5,11 +5,14 @@ DECL|macro|NOTES_INDENT
 DECL|macro|NOTES_INIT_EMPTY
 DECL|macro|NOTES_SHOW_HEADER
 DECL|member|combine_notes
+DECL|member|extra_notes_refs
 DECL|member|first_non_note
 DECL|member|initialized
 DECL|member|prev_non_note
 DECL|member|ref
 DECL|member|root
+DECL|member|suppress_default_notes
+DECL|struct|display_notes_opt
 DECL|struct|notes_tree
 DECL|typedef|combine_notes_fn
 DECL|typedef|each_note_fn
