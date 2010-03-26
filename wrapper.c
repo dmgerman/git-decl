f9f4133560505c536caeb106c2f7c486c4023a14
Peter Collingbourne <peter@pcc.me.uk>
2010-03-26 15:25:34 +0000

    Introduce remove_or_warn function
    This patch introduces the remove_or_warn function which is a
    generalised version of the {unlink,rmdir}_or_warn functions.  It takes
    an additional parameter indicating the mode of the file to be removed.
    
    The patch also modifies certain functions to use remove_or_warn
    where appropriate, and adds a test case for a bug fixed by the use
    of remove_or_warn.
    
    Signed-off-by: Peter Collingbourne <peter@pcc.me.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wrapper.c
+++ wrapper.c
@@ -4,6 +4,7 @@ DECL|function|git_inflate_init
 DECL|function|odb_mkstemp
 DECL|function|odb_pack_keep
 DECL|function|read_in_full
+DECL|function|remove_or_warn
 DECL|function|rmdir_or_warn
 DECL|function|unlink_or_warn
 DECL|function|warn_if_unremovable
