afc7346a046e51b586f9c83e9d86d2e46e3edef8
Christian Couder <chriscool@tuxfamily.org>
2010-03-31 21:22:05 +0200

    revert: refactor merge recursive code into its own function
    The code that is used to do a recursive merge is extracted from
    the revert_or_cherry_pick() function and put into a new
    do_recursive_merge() function.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/revert.c
+++ builtin/revert.c
@@ -4,6 +4,7 @@ DECL|function|add_message_to_msg
 DECL|function|cmd_cherry_pick
 DECL|function|cmd_revert
 DECL|function|die_dirty_index
+DECL|function|do_recursive_merge
 DECL|function|empty_tree
 DECL|function|free_message
 DECL|function|get_encoding
