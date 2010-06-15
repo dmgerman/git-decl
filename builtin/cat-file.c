f11e474dd62c6286e0892959f4910a72a0f18239
Clément Poulain <clement.poulain@ensimag.imag.fr>
2010-06-15 17:50:28 +0200

    textconv: support for cat_file
    Make the textconv_object function public, and add --textconv option to cat-file
    to perform conversion on blob objects. Using --textconv implies that we are
    working on a blob.
    As files drivers need to be initialized, a new config is required in addition
    to git_default_config. Therefore git_cat_file_config() is introduced
    
    Signed-off-by: Clément Poulain <clement.poulain@ensimag.imag.fr>
    Signed-off-by: Diane Gasselin <diane.gasselin@ensimag.imag.fr>
    Signed-off-by: Axel Bonnet <axel.bonnet@ensimag.imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -2,6 +2,7 @@ DECL|function|batch_objects
 DECL|function|batch_one_object
 DECL|function|cat_one_file
 DECL|function|cmd_cat_file
+DECL|function|git_cat_file_config
 DECL|function|pprint_tag
 DECL|macro|BATCH
 DECL|macro|BATCH_CHECK
