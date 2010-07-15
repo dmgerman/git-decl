6e400cd182b625bf3e34b8f04ed5ef9cf4454264
Jonathan Nieder <jrnieder@gmail.com>
2010-07-15 18:22:57 +0200

    Export parse_date_basic() to convert a date string to timestamp
    approxidate() is not appropriate for reading machine-written dates
    because it guesses instead of erroring out on malformed dates.
    parse_date() is less convenient since it returns its output as a
    string.  So export the underlying function that writes a timestamp.
    
    While at it, change the return value to match the usual convention:
    return 0 for success and -1 for failure.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Acked-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- date.c
+++ date.c
@@ -24,8 +24,8 @@ DECL|function|match_string
 DECL|function|match_tz
 DECL|function|nodate
 DECL|function|parse_date
+DECL|function|parse_date_basic
 DECL|function|parse_date_format
-DECL|function|parse_date_toffset
 DECL|function|pending_number
 DECL|function|show_date
 DECL|function|show_date_relative
