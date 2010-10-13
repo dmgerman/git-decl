8935c19e8724b61af05afb414a079f1c7b5eed35
Jonathan Nieder <jrnieder@gmail.com>
2010-10-13 04:39:44 -0500

    vcs-svn: implement copyfrom_data delta instruction
    The copyfrom_data instruction copies a few bytes verbatim from the
    novel text section of a window to the postimage.
    
    [jn: with memory leak fix from David]
    
    Improved-by: David Barr <david.barr@cordelta.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Acked-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- vcs-svn/svndiff.c
+++ vcs-svn/svndiff.c
@@ -1,5 +1,10 @@
 DECL|function|apply_one_window
+DECL|function|apply_window_in_core
+DECL|function|copyfrom_data
 DECL|function|error_short_read
+DECL|function|execute_one_instruction
+DECL|function|parse_first_operand
+DECL|function|parse_int
 DECL|function|read_chunk
 DECL|function|read_int
 DECL|function|read_length
@@ -7,10 +12,15 @@ DECL|function|read_magic
 DECL|function|read_offset
 DECL|function|svndiff0_apply
 DECL|function|window_release
+DECL|function|write_strbuf
+DECL|macro|INSN_COPYFROM_DATA
+DECL|macro|INSN_MASK
+DECL|macro|OPERAND_MASK
 DECL|macro|VLI_BITS_PER_DIGIT
 DECL|macro|VLI_CONTINUE
 DECL|macro|VLI_DIGIT_MASK
 DECL|macro|WINDOW_INIT
 DECL|member|data
 DECL|member|instructions
+DECL|member|out
 DECL|struct|window
