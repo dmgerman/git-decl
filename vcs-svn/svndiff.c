7c0d3c72ad2dbf3a77376a9d96252f56e3754fb3
Jonathan Nieder <jrnieder@gmail.com>
2010-10-13 04:21:43 -0500

    vcs-svn: parse svndiff0 window header
    Each window in a subversion delta (svndiff0-format file) starts with a
    window header, consisting of five integers with variable-length
    representation:
    
    	source view offset
    	source view length
    	output length
    	instructions length
    	auxiliary data length
    
    Parse it.  The result is not usable for deltas with nonempty postimage
    yet; in fact, this only adds support for deltas without any
    instructions or auxiliary data.  This is a good place to stop, though,
    since that little support lets us add some simple passing tests
    concerning error handling to the test suite.
    
    Improved-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Improved-by: David Barr <david.barr@cordelta.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- vcs-svn/svndiff.c
+++ vcs-svn/svndiff.c
@@ -1,3 +1,10 @@
+DECL|function|apply_one_window
 DECL|function|error_short_read
+DECL|function|read_int
+DECL|function|read_length
 DECL|function|read_magic
+DECL|function|read_offset
 DECL|function|svndiff0_apply
+DECL|macro|VLI_BITS_PER_DIGIT
+DECL|macro|VLI_CONTINUE
+DECL|macro|VLI_DIGIT_MASK
