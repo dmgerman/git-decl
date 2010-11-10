b0486759c9f5e8ad9f95c066e9be4d61c83e483c
Michael J Gruber <git@drmicha.warpmail.net>
2010-11-10 12:17:30 +0100

    tag: recognize rfc1991 signatures
    We have always been creating rfc1991 signatures for users with "rfc1991"
    in their gpg config but failed to recognize them (tag -l -n largenumber)
    and verify them (tag -v, verify-tag).
    
    Make good use of the refactored signature detection and let us recognize
    and verify those signatures also.
    
    Signed-off-by: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- tag.c
+++ tag.c
@@ -4,5 +4,6 @@ DECL|function|parse_signature
 DECL|function|parse_tag
 DECL|function|parse_tag_buffer
 DECL|function|parse_tag_date
+DECL|macro|PGP_MESSAGE
 DECL|macro|PGP_SIGNATURE
 DECL|variable|tag_type
