ab93688157b6205c0694f16b526e09b72008c602
Michael J Gruber <git@drmicha.warpmail.net>
2010-11-10 12:17:27 +0100

    verify-tag: factor out signature detection
    into tag.h/c for later reuse and modification.
    
    Signed-off-by: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- tag.c
+++ tag.c
@@ -1,6 +1,8 @@
 DECL|function|deref_tag
 DECL|function|lookup_tag
+DECL|function|parse_signature
 DECL|function|parse_tag
 DECL|function|parse_tag_buffer
 DECL|function|parse_tag_date
+DECL|macro|PGP_SIGNATURE
 DECL|variable|tag_type
