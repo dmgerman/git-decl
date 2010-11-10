1e6595b7685205320c29931baa87d504565007d0
Jens Lehmann <Jens.Lehmann@web.de>
2010-11-11 00:55:02 +0100

    Add the 'fetch.recurseSubmodules' config setting
    This new boolean option can be used to override the default for "git
    fetch" and "git pull", which is to not recurse into populated submodules
    and fetch all new commits there too.
    
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- submodule.c
+++ submodule.c
@@ -7,9 +7,11 @@ DECL|function|is_submodule_modified
 DECL|function|merge_submodule
 DECL|function|parse_submodule_config_option
 DECL|function|print_commit
+DECL|function|set_config_fetch_recurse_submodules
 DECL|function|set_diffopt_flags_from_submodule_config
 DECL|function|show_submodule_summary
 DECL|function|submodule_config
 DECL|macro|MERGE_WARNING
+DECL|variable|config_fetch_recurse_submodules
 DECL|variable|config_ignore_for_name
 DECL|variable|config_name_for_path
