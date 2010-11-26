b6243fa80d7b934e94b55c51d59db2c3071c07f8
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2010-11-26 22:32:33 +0700

    Add git_config_early()
    This version of git_config() will be used during repository setup.
    As a repository is being set up, $GIT_DIR is not nailed down yet,
    git_pathdup() should not be used to get $GIT_DIR/config.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -12,6 +12,7 @@ DECL|function|get_value
 DECL|function|git_config
 DECL|function|git_config_bool
 DECL|function|git_config_bool_or_int
+DECL|function|git_config_early
 DECL|function|git_config_from_file
 DECL|function|git_config_from_parameters
 DECL|function|git_config_global
