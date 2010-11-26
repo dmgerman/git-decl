1dd6dc252cd9287104c6702cd2d19da192e55944
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2010-11-26 22:31:57 +0700

    builtins: print setup info if repo is found
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- trace.c
+++ trace.c
@@ -1,5 +1,7 @@
 DECL|function|do_nothing
 DECL|function|get_trace_fd
+DECL|function|quote_crnl
 DECL|function|trace_argv_printf
 DECL|function|trace_printf
+DECL|function|trace_repo_setup
 DECL|variable|err_msg
