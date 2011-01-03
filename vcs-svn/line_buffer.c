0066f1ccfecf05defab77b0f5b35a1832c82065f
Jonathan Nieder <jrnieder@gmail.com>
2011-01-02 21:05:46 -0600

    vcs-svn: add binary-safe read function
    buffer_read_string works well for non line-oriented input except for
    one problem: it does not tell the caller how many bytes were actually
    written.  This means that unless one is very careful about checking
    for errors (and eof) the calling program cannot tell the difference
    between the string "foo" followed by an early end of file and the
    string "foo\0bar\0baz".
    
    So introduce a variant that reports the length, too, a thinner wrapper
    around strbuf_fread.  Its result is written to a strbuf so the caller
    does not need to keep track of the number of bytes read.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- vcs-svn/line_buffer.c
+++ vcs-svn/line_buffer.c
@@ -1,6 +1,7 @@
 DECL|function|buffer_copy_bytes
 DECL|function|buffer_deinit
 DECL|function|buffer_init
+DECL|function|buffer_read_binary
 DECL|function|buffer_read_line
 DECL|function|buffer_read_string
 DECL|function|buffer_reset
