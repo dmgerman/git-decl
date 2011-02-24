96f70a257d31de6fa9481345233f551fc0e8c977
Jeff King <peff@peff.net>
2011-02-24 09:28:41 -0500

    trace: refactor to support multiple env variables
    Right now you turn all tracing off and on with GIT_TRACE. To
    support new types of tracing without forcing the user to see
    all of them, we will soon support turning each tracing area
    on with GIT_TRACE_*.
    
    This patch lays the groundwork by providing an interface
    which does not assume GIT_TRACE. However, we still maintain
    the trace_printf interface so that existing callers do not
    need to be refactored.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- trace.c
+++ trace.c
@@ -2,6 +2,7 @@ DECL|function|get_trace_fd
 DECL|function|quote_crnl
 DECL|function|trace_argv_printf
 DECL|function|trace_printf
+DECL|function|trace_printf_key
 DECL|function|trace_repo_setup
 DECL|function|trace_vprintf
 DECL|variable|err_msg
