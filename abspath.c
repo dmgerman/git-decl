f48c30112c719ba1822f93c318baf92f99977888
Carlos Martín Nieto <cmn@elego.de>
2011-03-17 12:26:46 +0100

    Name make_*_path functions more accurately
    Rename the make_*_path functions so it's clearer what they do, in
    particlar make clear what the differnce between make_absolute_path and
    make_nonrelative_path is by renaming them real_path and absolute_path
    respectively. make_relative_path has an understandable name and is
    renamed to relative_path to maintain the name convention.
    
    The function calls have been replaced 1-to-1 in their usage.
    
    Signed-off-by: Carlos Martín Nieto <cmn@elego.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- abspath.c
+++ abspath.c
@@ -1,5 +1,5 @@
+DECL|function|absolute_path
 DECL|function|get_pwd_cwd
 DECL|function|is_directory
-DECL|function|make_absolute_path
-DECL|function|make_nonrelative_path
+DECL|function|real_path
 DECL|macro|MAXDEPTH
