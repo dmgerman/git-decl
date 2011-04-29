d2b1da5b4004d32ef62ea4f8fc22a06d03ea29fd
Johan Herland <johan@herland.net>
2011-04-29 11:36:19 +0200

    Add config variable for specifying default --dirstat behavior
    The new diff.dirstat config variable takes the same arguments as
    '--dirstat=<args>', and specifies the default arguments for --dirstat.
    The config is obviously overridden by --dirstat arguments passed on the
    command line.
    
    When not specified, the --dirstat defaults are 'changes,noncumulative,3'.
    
    The patch also adds several tests verifying the interaction between the
    diff.dirstat config variable, and the --dirstat command line option.
    
    Improved-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff.c
+++ diff.c
@@ -218,6 +218,7 @@ DECL|variable|degrade_cc_to_c_warning
 DECL|variable|diff_auto_refresh_index
 DECL|variable|diff_colors
 DECL|variable|diff_detect_rename_default
+DECL|variable|diff_dirstat_percent_default
 DECL|variable|diff_mnemonic_prefix
 DECL|variable|diff_no_prefix
 DECL|variable|diff_queued_diff
