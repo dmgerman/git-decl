6784d0892ffe9bf5839c8a63bb78ba7752e542b2
Jeff King <peff@peff.net>
2011-05-16 02:52:11 -0400

    connect: let callers know if connection is a socket
    They might care because they want to do a half-duplex close.
    With pipes, that means simply closing the output descriptor;
    with a socket, you must actually call shutdown.
    
    Instead of exposing the magic no_fork child_process struct,
    let's encapsulate the test in a function.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- connect.c
+++ connect.c
@@ -11,6 +11,7 @@ DECL|function|get_port
 DECL|function|get_protocol
 DECL|function|get_remote_heads
 DECL|function|git_connect
+DECL|function|git_connection_is_socket
 DECL|function|git_proxy_command_options
 DECL|function|git_proxy_connect
 DECL|function|git_tcp_connect
