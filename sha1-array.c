4bdaea27b47cdc5341a1bf1ebcc99dfb2ef6a35d
Jeff King <peff@peff.net>
2011-05-19 17:34:33 -0400

    bisect: refactor sha1_array into a generic sha1 list
    This is a generally useful abstraction, so let's let others
    make use of it.  The refactoring is more or less a straight
    copy; however, functions and struct members have had their
    names changed to match string_list, which is the most
    similar data structure.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ sha1-array.c
@@ -0,0 +1,6 @@
+DECL|function|sha1_access
+DECL|function|sha1_array_append
+DECL|function|sha1_array_clear
+DECL|function|sha1_array_lookup
+DECL|function|sha1_array_sort
+DECL|function|void_hashcmp
---
+++ sha1-array.h
@@ -0,0 +1,7 @@
+DECL|macro|SHA1_ARRAY_H
+DECL|macro|SHA1_ARRAY_INIT
+DECL|member|alloc
+DECL|member|nr
+DECL|member|sha1
+DECL|member|sorted
+DECL|struct|sha1_array
