8b16d0c4a99f29576abea722cdfb4efd0b522a29
Junio C Hamano <gitster@pobox.com>
2011-06-03 15:32:17 -0700

    verify-pack: use index-pack --verify
    This finally gets rid of the inefficient verify-pack implementation that
    walks objects in the packfile in their object name order and replaces it
    with a call to index-pack --verify. As a side effect, it also removes
    packed_object_info_detail() API which is rather expensive.
    
    As this changes the way errors are reported (verify-pack used to rely on
    the usual runtime error detection routine unpack_entry() to diagnose the
    CRC errors in an entry in the *.idx file; index-pack --verify checks the
    whole *.idx file in one go), update a test that expected the string "CRC"
    to appear in the error message.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/verify-pack.c
+++ builtin/verify-pack.c
@@ -1,7 +1,5 @@
 DECL|function|cmd_verify_pack
-DECL|function|show_pack_info
 DECL|function|verify_one_pack
-DECL|macro|MAX_CHAIN
 DECL|macro|VERIFY_PACK_STAT_ONLY
 DECL|macro|VERIFY_PACK_VERBOSE
 DECL|variable|verify_pack_usage
