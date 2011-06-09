4b225a25787a3e97bbded8562105e83705b971e1
Jeff King <peff@peff.net>
2011-06-09 11:55:23 -0400

    parse-options: add OPT_STRING_LIST helper
    This just adds repeated invocations of an option to a list
    of strings. Using the "--no-<var>" form will reset the list
    to empty.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.c
+++ parse-options.c
@@ -9,6 +9,7 @@ DECL|function|parse_nodash_opt
 DECL|function|parse_opt_abbrev_cb
 DECL|function|parse_opt_approxidate_cb
 DECL|function|parse_opt_color_flag_cb
+DECL|function|parse_opt_string_list
 DECL|function|parse_opt_tertiary
 DECL|function|parse_opt_verbosity_cb
 DECL|function|parse_opt_with_commit
