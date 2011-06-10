2242573b816ca2c2bd03ce0257163bc3490a31b2
Junio C Hamano <gitster@pobox.com>
2011-06-10 10:55:10 -0700

    zlib: wrap deflate side of the API
    Wrap deflateInit, deflate, and deflateEnd for everybody, and the sole use
    of deflateInit2 in remote-curl.c to tell the library to use gzip header
    and trailer in git_deflate_init_gzip().
    
    There is only one caller that cares about the status from deflateEnd().
    Introduce git_deflate_end_gently() to let that sole caller retrieve the
    status and act on it (i.e. die) for now, but we would probably want to
    make inflate_end/deflate_end die when they ran out of memory and get
    rid of the _gently() kind.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- zlib.c
+++ zlib.c
@@ -1,3 +1,8 @@
+DECL|function|git_deflate
+DECL|function|git_deflate_end
+DECL|function|git_deflate_end_gently
+DECL|function|git_deflate_init
+DECL|function|git_deflate_init_gzip
 DECL|function|git_inflate
 DECL|function|git_inflate_end
 DECL|function|git_inflate_init
