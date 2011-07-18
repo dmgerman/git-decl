31c0ca067f18757db78e4111d83f1464dadfbbf6
Jeff King <peff@peff.net>
2011-07-18 03:48:51 -0400

    url: decode buffers that are not NUL-terminated
    The url_decode function needs only minor tweaks to handle
    arbitrary buffers. Let's do those tweaks, which cleans up an
    unreadable mess of temporary strings in http.c.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- url.c
+++ url.c
@@ -5,5 +5,6 @@ DECL|function|str_end_url_with_slash
 DECL|function|url_decode
 DECL|function|url_decode_char
 DECL|function|url_decode_internal
+DECL|function|url_decode_mem
 DECL|function|url_decode_parameter_name
 DECL|function|url_decode_parameter_value
