3ad5d0b8d93442568cbb0a335fd14f346c4084e4
Clemens Buchacher <drizzd@aon.at>
2011-07-27 23:32:34 +0200

    error_routine: use parent's stderr if exec fails
    The new process's error output may be redirected elsewhere, but if
    the exec fails, output should still go to the parent's stderr. This
    has already been done for the die_routine. Do the same for
    error_routine.
    
    Signed-off-by: Clemens Buchacher <drizzd@aon.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- usage.c
+++ usage.c
@@ -4,10 +4,12 @@ DECL|function|die_errno
 DECL|function|error
 DECL|function|error_builtin
 DECL|function|set_die_routine
+DECL|function|set_error_routine
 DECL|function|usage
 DECL|function|usage_builtin
 DECL|function|usagef
 DECL|function|vreportf
+DECL|function|vwritef
 DECL|function|warn_builtin
 DECL|function|warning
 DECL|variable|die_routine
