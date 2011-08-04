470cda46849052c7a99861cdb0ae77e36f512491
Michael Haggerty <mhagger@alum.mit.edu>
2011-08-04 06:36:24 +0200

    git-check-attr: Extract a function output_attr()
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/check-attr.c
+++ builtin/check-attr.c
@@ -1,6 +1,7 @@
 DECL|function|check_attr
 DECL|function|check_attr_stdin_paths
 DECL|function|cmd_check_attr
+DECL|function|output_attr
 DECL|variable|check_attr_options
 DECL|variable|check_attr_usage
 DECL|variable|null_term_line
