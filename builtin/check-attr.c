0dba93bae210ffb19cfc222023e16c6c3e54676b
Michael Haggerty <mhagger@alum.mit.edu>
2011-08-04 06:36:26 +0200

    git-check-attr: Extract a function error_with_usage()
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/check-attr.c
+++ builtin/check-attr.c
@@ -1,6 +1,7 @@
 DECL|function|check_attr
 DECL|function|check_attr_stdin_paths
 DECL|function|cmd_check_attr
+DECL|function|error_with_usage
 DECL|function|output_attr
 DECL|variable|check_attr_options
 DECL|variable|check_attr_usage
