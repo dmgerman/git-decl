872c9937672edf587283976034fdec1bf171100f
Michael Haggerty <mhagger@alum.mit.edu>
2011-09-15 23:10:26 +0200

    Refactor check_refname_format()
    Among other things, extract a function check_refname_component().
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -1,6 +1,7 @@
 DECL|function|add_extra_ref
 DECL|function|add_ref
 DECL|function|bad_ref_char
+DECL|function|check_refname_component
 DECL|function|check_refname_format
 DECL|function|clear_extra_refs
 DECL|function|close_ref
