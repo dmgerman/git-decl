e7609639c4d2ec90d2c13733d1cc2a1e2fd23d6e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2011-09-17 21:57:44 +1000

    merge: remove global variable head[]
    Also kill head_invalid in favor of "head_commit == NULL".
    
    Local variable "head" in cmd_merge() is renamed to "head_sha1" to make
    sure I don't miss any access because this variable should not be used
    after head_commit is set (use head_commit->object.sha1 instead).
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -54,7 +54,6 @@ DECL|variable|builtin_merge_usage
 DECL|variable|default_to_upstream
 DECL|variable|fast_forward_only
 DECL|variable|have_message
-DECL|variable|head
 DECL|variable|merge_msg
 DECL|variable|option_commit
 DECL|variable|option_renormalize
