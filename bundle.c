415dd41e2cd3a5544bcc6e5065e200a077141488
Junio C Hamano <gitster@pobox.com>
2011-10-13 15:12:02 -0700

    bundle: allowing to read from an unseekable fd
    We wished that "git bundle" to eventually learn to read from a network
    socket which is not seekable. The current code opens with fopen(), reads
    the file halfway and run ftell(), and reopens the same file with open()
    and seeks, to skip the header.
    
    This patch by itself does not reach that goal yet, but I think it is a
    right step in that direction.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- bundle.c
+++ bundle.c
@@ -4,6 +4,7 @@ DECL|function|is_tag_in_date_range
 DECL|function|list_bundle_refs
 DECL|function|list_refs
 DECL|function|read_bundle_header
+DECL|function|strbuf_readline_fd
 DECL|function|unbundle
 DECL|function|verify_bundle
 DECL|macro|PREREQ_MARK
