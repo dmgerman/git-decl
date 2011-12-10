ccd279b9b4faef3ca1e4c162f0f3a5324563858f
Jeff King <peff@peff.net>
2011-12-10 05:34:14 -0500

    credentials: add "cache" helper
    If you access repositories over smart-http using http
    authentication, then it can be annoying to have git ask you
    for your password repeatedly. We cache credentials in
    memory, of course, but git is composed of many small
    programs. Having to input your password for each one can be
    frustrating.
    
    This patch introduces a credential helper that will cache
    passwords in memory for a short period of time.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ credential-cache.c
@@ -0,0 +1,6 @@
+DECL|function|do_cache
+DECL|function|main
+DECL|function|send_request
+DECL|function|spawn_daemon
+DECL|macro|FLAG_RELAY
+DECL|macro|FLAG_SPAWN
---
+++ unix-socket.h
@@ -0,0 +1,1 @@
+DECL|macro|UNIX_SOCKET_H
