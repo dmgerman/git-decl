557260fe1381b4e8bcae8670ffe4f9821d6e9bcf
Junio C Hamano <gitster@pobox.com>
2012-07-02 12:43:05 -0700

    revision.c: the "log" family, except for "show", takes committish
    Add a field to setup_revision_opt structure and allow these callers
    to tell the setup_revisions command parsing machinery that short SHA1
    it encounters are meant to name committish.
    
    This step does not go all the way to connect the setup_revisions()
    to sha1_name.c yet.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.h
+++ revision.h
@@ -15,6 +15,7 @@ DECL|macro|DECORATE_FULL_REFS
 DECL|macro|DECORATE_SHORT_REFS
 DECL|macro|PATCHSAME
 DECL|macro|REVARG_CANNOT_BE_FILENAME
+DECL|macro|REVARG_COMMITTISH
 DECL|macro|REVISION_H
 DECL|macro|REV_TREE_DIFFERENT
 DECL|macro|REV_TREE_NEW
@@ -107,6 +108,7 @@ DECL|member|ref_message_ids
 DECL|member|reflog_info
 DECL|member|remove_empty_trees
 DECL|member|rev
+DECL|member|revarg_opt
 DECL|member|reverse
 DECL|member|reverse_output_stage
 DECL|member|rewrite_parents
