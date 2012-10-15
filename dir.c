becf74b666a3b7ca42fd944fbf8ea6c3a53861cc
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-10-15 13:24:37 +0700

    exclude: split pathname matching code into a separate function
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- dir.c
+++ dir.c
@@ -39,6 +39,7 @@ DECL|function|is_empty_dir
 DECL|function|is_inside_dir
 DECL|function|match_basename
 DECL|function|match_one
+DECL|function|match_pathname
 DECL|function|match_pathspec
 DECL|function|match_pathspec_depth
 DECL|function|match_pathspec_item
