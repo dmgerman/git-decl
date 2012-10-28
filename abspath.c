d0b60d340403c56301a71ad3b0bc8e705fb30dee
Michael Haggerty <mhagger@alum.mit.edu>
2012-10-28 17:16:22 +0100

    Introduce new function real_path_if_valid()
    The function is like real_path(), except that it returns NULL on error
    instead of dying.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Jeff King <peff@peff.net>

--- abspath.c
+++ abspath.c
@@ -3,5 +3,6 @@ DECL|function|get_pwd_cwd
 DECL|function|is_directory
 DECL|function|prefix_filename
 DECL|function|real_path
+DECL|function|real_path_if_valid
 DECL|function|real_path_internal
 DECL|macro|MAXDEPTH
