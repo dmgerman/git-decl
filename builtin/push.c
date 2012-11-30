fda9bac87a3582bd1dec73124749ccc453197f99
Chris Rorvick <chris@rorvick.com>
2012-11-29 19:41:34 -0600

    push: add advice for rejected tag reference
    Advising the user to fetch and merge only makes sense if the rejected
    reference is a branch.  If none of the rejections are for branches, just
    tell the user the reference already exists.
    
    Signed-off-by: Chris Rorvick <chris@rorvick.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/push.c
+++ builtin/push.c
@@ -1,6 +1,7 @@
 DECL|function|add_refspec
 DECL|function|advise_checkout_pull_push
 DECL|function|advise_pull_before_push
+DECL|function|advise_ref_already_exists
 DECL|function|advise_use_upstream
 DECL|function|cmd_push
 DECL|function|die_push_simple
@@ -16,6 +17,7 @@ DECL|variable|default_matching_used
 DECL|variable|deleterefs
 DECL|variable|message_advice_checkout_pull_push
 DECL|variable|message_advice_pull_before_push
+DECL|variable|message_advice_ref_already_exists
 DECL|variable|message_advice_use_upstream
 DECL|variable|progress
 DECL|variable|push_usage
--- cache.h
+++ cache.h
@@ -333,6 +333,7 @@ DECL|member|next
 DECL|member|next_in
 DECL|member|next_out
 DECL|member|nonfastforward
+DECL|member|not_forwardable
 DECL|member|not_new
 DECL|member|nr
 DECL|member|nr
