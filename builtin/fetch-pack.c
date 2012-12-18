344554ad1fc23501527b6213f1ce410a7dba1722
Matt Kraai <matt.kraai@amo.abbott.com>
2012-12-18 14:03:54 -0800

    Make lock local to fetch_pack
    lock is only used by fetch_pack, so move it into that function.
    
    Signed-off-by: Matt Kraai <matt.kraai@amo.abbott.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/fetch-pack.c
+++ builtin/fetch-pack.c
@@ -49,7 +49,6 @@ DECL|variable|complete
 DECL|variable|fetch_fsck_objects
 DECL|variable|fetch_pack_usage
 DECL|variable|fetch_unpack_limit
-DECL|variable|lock
 DECL|variable|marked
 DECL|variable|multi_ack
 DECL|variable|no_done
