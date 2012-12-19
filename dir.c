fdb1d92c430ff602d7190c82005cb604af7623bb
Jeff King <peff@peff.net>
2012-12-19 17:37:30 -0500

    add global --literal-pathspecs option
    Git takes pathspec arguments in many places to limit the
    scope of an operation. These pathspecs are treated not as
    literal paths, but as glob patterns that can be fed to
    fnmatch. When a user is giving a specific pattern, this is a
    nice feature.
    
    However, when programatically providing pathspecs, it can be
    a nuisance. For example, to find the latest revision which
    modified "$foo", one can use "git rev-list -- $foo". But if
    "$foo" contains glob characters (e.g., "f*"), it will
    erroneously match more entries than desired. The caller
    needs to quote the characters in $foo, and even then, the
    results may not be exactly the same as with a literal
    pathspec. For instance, the depth checks in
    match_pathspec_depth do not kick in if we match via fnmatch.
    
    This patch introduces a global command-line option (i.e.,
    one for "git" itself, not for specific commands) to turn
    this behavior off. It also has a matching environment
    variable, which can make it easier if you are a script or
    porcelain interface that is going to issue many such
    commands.
    
    This option cannot turn off globbing for particular
    pathspecs. That could eventually be done with a ":(noglob)"
    magic pathspec prefix. However, that level of granularity is
    more cumbersome to use for many cases, and doing ":(noglob)"
    right would mean converting the whole codebase to use
    "struct pathspec", as the usual "const char **pathspec"
    cannot represent extra per-item flags.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- dir.c
+++ dir.c
@@ -37,6 +37,7 @@ DECL|function|get_index_dtype
 DECL|function|init_pathspec
 DECL|function|is_empty_dir
 DECL|function|is_inside_dir
+DECL|function|limit_pathspec_to_literal
 DECL|function|match_basename
 DECL|function|match_one
 DECL|function|match_pathname
