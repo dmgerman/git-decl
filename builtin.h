db39e09084b2fe1eede2b8169e729f8c5cb2b20e
Junio C Hamano <gitster@pobox.com>
2012-12-28 15:29:31 -0800

    merge --no-edit: do not credit people involved in the side branch
    The credit lines "By" and "Via" to credit authors and committers for
    their contributions on the side branch are meant as a hint to the
    integrator to decide whom to mention in the log message text.  After
    the integrator saves the message in the editor, they are meant to go
    away and that is why they are commented out.
    
    When a merge is recorded without editing the generated message,
    however, its contents do not go through the normal stripspace()
    and these lines are left in the merge.
    
    Stop producing them when we know the merge is going to be recorded
    without editing, i.e. when --no-edit is given.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin.h
+++ builtin.h
@@ -3,6 +3,7 @@ DECL|macro|DEFAULT_MERGE_LOG_LEN
 DECL|member|add_title
 DECL|member|cmd
 DECL|member|combine
+DECL|member|credit_people
 DECL|member|enabled
 DECL|member|mode_from_env
 DECL|member|refs
