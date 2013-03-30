60c9c0afb303138aa083341e6b6b42db10793996
Torsten Bögershausen <tboegi@web.de>
2013-03-30 10:53:32 +0100

    path.c: simplify adjust_shared_perm()
    All calls to set_shared_perm() use mode == 0, so simplify the
    function.
    
    Because all callers use the macro adjust_shared_perm(path) from
    cache.h to call this function, convert it to a proper function,
    losing set_shared_perm().
    
    Since path.c has much more functions than just mkpath() these days,
    drop the stale comment about it.
    
    Signed-off-by: Torsten Bögershausen <tboegi@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- path.c
+++ path.c
@@ -1,3 +1,4 @@
+DECL|function|adjust_shared_perm
 DECL|function|chomp_trailing_dir_sep
 DECL|function|cleanup_path
 DECL|function|daemon_avoid_alias
@@ -18,7 +19,6 @@ DECL|function|mksnpath
 DECL|function|normalize_path_copy
 DECL|function|offset_1st_component
 DECL|function|relative_path
-DECL|function|set_shared_perm
 DECL|function|strip_path_suffix
 DECL|function|validate_headref
 DECL|function|vsnpath
