bb475f798df416d7ff10ccf09a1ecf1bdafa125e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-04-19 09:08:45 +1000

    utf8.c: add utf8_strnwidth() with the ability to skip ansi sequences
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- utf8.c
+++ utf8.c
@@ -13,6 +13,7 @@ DECL|function|strbuf_add_wrapped_bytes
 DECL|function|strbuf_add_wrapped_text
 DECL|function|strbuf_addchars
 DECL|function|utf8_fprintf
+DECL|function|utf8_strnwidth
 DECL|function|utf8_strwidth
 DECL|function|utf8_width
 DECL|member|first
