b7706eda898f5309fea27f7e54951a043bad9393
Michael Haggerty <mhagger@alum.mit.edu>
2013-04-22 21:52:34 +0200

    refs: use same lock_file object for both ref-packing functions
    Use a single struct lock_file for both pack_refs() and
    repack_without_ref().
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -177,7 +177,6 @@ DECL|struct|warn_if_dangling_data
 DECL|typedef|each_ref_entry_fn
 DECL|variable|PACKED_REFS_HEADER
 DECL|variable|current_ref
-DECL|variable|packed
 DECL|variable|packlock
 DECL|variable|ref_cache
 DECL|variable|ref_rev_parse_rules
