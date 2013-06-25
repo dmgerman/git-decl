4b9d89309c59951091972dc22a10e018b07977a5
Jiang Xin <worldhello.net@gmail.com>
2013-06-25 23:53:48 +0800

    git-clean: add support for -i/--interactive
    Show what would be done and the user must confirm before actually
    cleaning.
    
        Would remove ...
        Would remove ...
        Would remove ...
    
        Remove [y/n]?
    
    Press "y" to start cleaning, and press "n" if you want to abort.
    
    Signed-off-by: Jiang Xin <worldhello.net@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clean.c
+++ builtin/clean.c
@@ -1,10 +1,12 @@
 DECL|function|cmd_clean
 DECL|function|exclude_cb
 DECL|function|git_clean_config
+DECL|function|interactive_main_loop
 DECL|function|remove_dirs
 DECL|variable|builtin_clean_usage
 DECL|variable|del_list
 DECL|variable|force
+DECL|variable|interactive
 DECL|variable|msg_remove
 DECL|variable|msg_skip_git_dir
 DECL|variable|msg_warn_remove_failed
