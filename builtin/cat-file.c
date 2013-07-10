5f29fe6ea4ed199c8e7d7ce7f4d7313b4b3e60a6
Jeff King <peff@peff.net>
2013-07-10 07:38:58 -0400

    cat-file: refactor --batch option parsing
    We currently use an int to tell us whether --batch parsing
    is on, and if so, whether we should print the full object
    contents. Let's instead factor this into a struct, filled in
    by callback, which will make further batch-related options
    easy to add.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -1,9 +1,11 @@
 DECL|function|batch_objects
 DECL|function|batch_one_object
+DECL|function|batch_option_callback
 DECL|function|cat_one_file
 DECL|function|cmd_cat_file
 DECL|function|git_cat_file_config
 DECL|function|print_object_or_die
-DECL|macro|BATCH
-DECL|macro|BATCH_CHECK
+DECL|member|enabled
+DECL|member|print_contents
+DECL|struct|batch_options
 DECL|variable|cat_file_usage
