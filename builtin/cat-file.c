7d2cac8cddea2d6b14129cf54774b0b0ae59fa58
Jeff King <peff@peff.net>
2013-07-10 07:38:24 -0400

    cat-file: teach --batch to stream blob objects
    The regular "git cat-file -p" and "git cat-file blob" code
    paths already learned to stream large blobs. Let's do the
    same here.
    
    Note that this means we look up the type and size before
    making a decision of whether to load the object into memory
    or stream (just like the "-p" code path does). That can lead
    to extra work, but it should be dwarfed by the cost of
    actually accessing the object itself. In my measurements,
    there was a 1-2% slowdown when using "--batch" on a large
    number of objects.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -3,6 +3,7 @@ DECL|function|batch_one_object
 DECL|function|cat_one_file
 DECL|function|cmd_cat_file
 DECL|function|git_cat_file_config
+DECL|function|print_object_or_die
 DECL|macro|BATCH
 DECL|macro|BATCH_CHECK
 DECL|variable|cat_file_usage
