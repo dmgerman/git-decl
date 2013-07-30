c3ed9647a2959e333f0a2c304f90be76383da411
Jeremy Huddleston <jeremyhu@apple.com>
2013-07-29 18:28:30 -0700

    imap-send: use Apple's Security framework for base64 encoding
    Use Apple's supported functions for base64 encoding instead
    of the deprecated OpenSSL functions.
    
    Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>
    Signed-off-by: David Aguilar <davvid@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ compat/apple-common-crypto.h
@@ -0,0 +1,16 @@
+DECL|function|git_CC_EVP_DecodeBlock
+DECL|function|git_CC_EVP_EncodeBlock
+DECL|macro|APPLE_LION_OR_NEWER
+DECL|macro|EVP_DecodeBlock
+DECL|macro|EVP_EncodeBlock
+DECL|macro|EVP_md5
+DECL|macro|HEADER_HMAC_H
+DECL|macro|HEADER_SHA_H
+DECL|macro|HMAC_CTX
+DECL|macro|HMAC_CTX_cleanup
+DECL|macro|HMAC_Final
+DECL|macro|HMAC_Init
+DECL|macro|HMAC_Update
+DECL|macro|OPENSSL_NO_MD5
+DECL|macro|TYPE_BOOL
+DECL|macro|git_CC_error_check
