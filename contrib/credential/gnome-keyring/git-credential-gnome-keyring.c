0ff81588041dd1eb85746f48c3b00015316c2bea
Brandon Casey <drafnel@gmail.com>
2013-09-23 11:49:16 -0700

    contrib/git-credential-gnome-keyring.c: support ancient gnome-keyring
    The gnome-keyring lib distributed with RHEL 5.X is ancient and does
    not provide a few of the functions/defines that more recent versions
    do, but mostly the API is the same.  Let's provide the missing bits
    via macro definitions and function implementation.
    
    Signed-off-by: Brandon Casey <drafnel@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/credential/gnome-keyring/git-credential-gnome-keyring.c
+++ contrib/credential/gnome-keyring/git-credential-gnome-keyring.c
@@ -3,6 +3,7 @@ DECL|function|credential_init
 DECL|function|credential_read
 DECL|function|credential_write
 DECL|function|credential_write_item
+DECL|function|gnome_keyring_result_to_message
 DECL|function|keyring_erase
 DECL|function|keyring_get
 DECL|function|keyring_object
@@ -11,6 +12,11 @@ DECL|function|main
 DECL|function|usage
 DECL|macro|CREDENTIAL_INIT
 DECL|macro|CREDENTIAL_OP_END
+DECL|macro|GNOME_KEYRING_DEFAULT
+DECL|macro|GNOME_KEYRING_RESULT_NO_MATCH
+DECL|macro|gnome_keyring_memory_alloc
+DECL|macro|gnome_keyring_memory_free
+DECL|macro|gnome_keyring_memory_strdup
 DECL|member|host
 DECL|member|name
 DECL|member|op
