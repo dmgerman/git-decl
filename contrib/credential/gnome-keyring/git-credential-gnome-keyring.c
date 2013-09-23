7e0b182d02d11b4528fbb74a0a28f0fa5521980c
Brandon Casey <drafnel@gmail.com>
2013-09-23 11:49:14 -0700

    contrib/git-credential-gnome-keyring.c: use glib messaging functions
    Rather than roll our own, let's use the messaging functions provided
    by glib.
    
    Signed-off-by: Brandon Casey <drafnel@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/credential/gnome-keyring/git-credential-gnome-keyring.c
+++ contrib/credential/gnome-keyring/git-credential-gnome-keyring.c
@@ -3,14 +3,12 @@ DECL|function|credential_init
 DECL|function|credential_read
 DECL|function|credential_write
 DECL|function|credential_write_item
-DECL|function|error
 DECL|function|keyring_erase
 DECL|function|keyring_get
 DECL|function|keyring_object
 DECL|function|keyring_store
 DECL|function|main
 DECL|function|usage
-DECL|function|warning
 DECL|macro|CREDENTIAL_INIT
 DECL|macro|CREDENTIAL_OP_END
 DECL|member|host
