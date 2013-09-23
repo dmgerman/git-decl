e2c82d081d749605c758881f72dcb8d4e6a96d51
Brandon Casey <drafnel@gmail.com>
2013-09-23 11:49:17 -0700

    contrib/git-credential-gnome-keyring.c: support really ancient gnome-keyring
    The gnome-keyring lib (0.4) distributed with RHEL 4.X is really ancient
    and does not provide most of the synchronous functions that even ancient
    releases do.  Thankfully, we're only using one function that is missing.
    Let's emulate gnome_keyring_item_delete_sync() by calling the asynchronous
    function and then triggering the event loop processing until our
    callback is called.
    
    Signed-off-by: Brandon Casey <drafnel@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/credential/gnome-keyring/git-credential-gnome-keyring.c
+++ contrib/credential/gnome-keyring/git-credential-gnome-keyring.c
@@ -3,6 +3,8 @@ DECL|function|credential_init
 DECL|function|credential_read
 DECL|function|credential_write
 DECL|function|credential_write_item
+DECL|function|gnome_keyring_done_cb
+DECL|function|gnome_keyring_item_delete_sync
 DECL|function|gnome_keyring_result_to_message
 DECL|function|keyring_erase
 DECL|function|keyring_get
@@ -10,6 +12,7 @@ DECL|function|keyring_object
 DECL|function|keyring_store
 DECL|function|main
 DECL|function|usage
+DECL|function|wait_for_request_completion
 DECL|macro|CREDENTIAL_INIT
 DECL|macro|CREDENTIAL_OP_END
 DECL|macro|GNOME_KEYRING_DEFAULT
