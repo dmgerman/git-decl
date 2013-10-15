1e847dabb8606d39547effc3aa4b262f66b8a5d5
Jeff King <peff@peff.net>
2013-10-14 20:06:14 -0400

    http: use curl's tcp keepalive if available
    Commit a15d069 taught git to use curl's SOCKOPTFUNCTION hook
    to turn on TCP keepalives. However, modern versions of curl
    have a TCP_KEEPALIVE option, which can do this for us. As an
    added bonus, the curl code knows how to turn on keepalive
    for a much wider variety of platforms. The only downside to
    using this option is that not everybody has a new enough curl.
    Let's split our keepalive options into three conditionals:
    
      1. With curl 7.25.0 and newer, we rely on curl to do it
         right.
    
      2. With older curl that still knows SOCKOPTFUNCTION, we
         use the code from a15d069.
    
      3. Otherwise, we are out of luck, and the call is a no-op.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http.c
+++ http.c
@@ -40,6 +40,9 @@ DECL|function|release_active_slot
 DECL|function|release_http_object_request
 DECL|function|release_http_pack_request
 DECL|function|run_active_slot
+DECL|function|set_curl_keepalive
+DECL|function|set_curl_keepalive
+DECL|function|set_curl_keepalive
 DECL|function|set_from_env
 DECL|function|sockopt_callback
 DECL|function|start_active_slot
