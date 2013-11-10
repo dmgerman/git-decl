7c596e781ca73b727914167c599fd22aba397251
John Keeping <john@keeping.me.uk>
2013-11-10 15:47:29 +0000

    tar-tree: remove deprecated command
    "git tar-tree" has been a thin wrapper around "git archive" since commit
    fd88d9c (Remove upload-tar and make git-tar-tree a thin wrapper to
    git-archive, 2006-09-24), which also made it print a message indicating
    that git-tar-tree is deprecated.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/tar-tree.c
+++ builtin/tar-tree.c
@@ -1,6 +1,4 @@
 DECL|function|cmd_get_tar_commit_id
-DECL|function|cmd_tar_tree
 DECL|macro|HEADERSIZE
 DECL|macro|RECORDSIZE
 DECL|variable|builtin_get_tar_commit_id_usage
-DECL|variable|tar_tree_usage
