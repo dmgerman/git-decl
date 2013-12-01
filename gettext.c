ba582bf9829202804fe3e890105cc77af45c4733
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-12-01 09:45:38 +0700

    gettext.c: detect the vsnprintf bug at runtime
    Bug 6530 [1] in glibc causes "git show v0.99.6~1" to fail with error
    "your vsnprintf is broken". The workaround avoids that, but it
    corrupts system error messages in non-C locales.
    
    The bug has been fixed since 2.17. We could know running glibc version
    with gnu_get_libc_version(). But version is not a sure way to detect
    the bug because downstream may back port the fix to older versions. Do
    a runtime test that immitates the call flow that leads to "your
    vsnprintf is broken". Only enable the workaround if the test fails.
    
    Tested on Gentoo Linux, glibc 2.16.0 and 2.17, amd64.
    
    [1] http://sourceware.org/bugzilla/show_bug.cgi?id=6530
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- gettext.c
+++ gettext.c
@@ -1,6 +1,7 @@
 DECL|function|gettext_width
 DECL|function|git_setup_gettext
 DECL|function|init_gettext_charset
+DECL|function|test_vsnprintf
 DECL|function|use_gettext_poison
 DECL|macro|locale_charset
 DECL|variable|charset
