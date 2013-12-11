b3e2c838547c29b859eb5211206b94405ac1b1fc
Christian Couder <chriscool@tuxfamily.org>
2013-12-11 08:46:10 +0100

    builtin/replace: teach listing using short, medium or full formats
    By default when listing replace refs, only the sha1 of the
    replaced objects are shown.
    
    In many cases, it is much nicer to be able to list all the
    sha1 of the replaced objects along with the sha1 of the
    replacment objects.
    
    And in other cases it might be interesting to also show the
    types of the replaced and replacement objects.
    
    This patch introduce a new --format=<fmt> option where
    <fmt> can be any of the following:
    
    	'short': this is the same as when no --format
    		option is used, that is only the sha1 of
    		the replaced objects are shown
    	'medium': this also lists the sha1 of the
    		replacement objects
    	'full': this shows the sha1 and the type of both
    		the replaced and the replacement objects
    
    Some documentation and some tests will follow.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/replace.c
+++ builtin/replace.c
@@ -1,8 +1,15 @@
+DECL|enumerator|FULL
+DECL|enumerator|MEDIUM
+DECL|enumerator|SHORT
+DECL|enum|repl_fmt
 DECL|function|cmd_replace
 DECL|function|delete_replace_ref
 DECL|function|for_each_replace_name
 DECL|function|list_replace_refs
 DECL|function|replace_object
 DECL|function|show_reference
+DECL|member|fmt
+DECL|member|pattern
+DECL|struct|show_data
 DECL|typedef|each_replace_name_fn
 DECL|variable|git_replace_usage
