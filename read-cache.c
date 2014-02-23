fd83f0c90011a436bb1b3c1f6c7483cb50059fed
Thomas Gummerer <t.gummerer@gmail.com>
2014-02-23 21:49:59 +0100

    read-cache: add index.version config variable
    Add a config variable that allows setting the default index version when
    initializing a new index file.  Similar to the GIT_INDEX_VERSION
    environment variable this only affects new index files.
    
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- read-cache.c
+++ read-cache.c
@@ -35,6 +35,7 @@ DECL|function|has_file_name
 DECL|function|has_racy_timestamp
 DECL|function|ie_match_stat
 DECL|function|ie_modified
+DECL|function|index_format_config
 DECL|function|index_name_is_other
 DECL|function|index_name_pos
 DECL|function|index_name_pos_also_unmerged
