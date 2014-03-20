d752cb9299736447f21e71cb536a79599ef56f93
Matthieu Moy <Matthieu.Moy@imag.fr>
2014-03-20 13:12:41 +0100

    status: disable translation when --porcelain is used
    "git status --branch --porcelain" displays the status of the branch
    (ahead, behind, gone), and used gettext to translate the string.
    
    Use hardcoded strings when --porcelain is used, but keep the gettext
    translation for "git status --short" which is essentially the same, but
    meant to be read by a human.
    
    Reported-by: Anarky <ghostanarky@gmail.com>
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.c
+++ wt-status.c
@@ -49,6 +49,7 @@ DECL|function|wt_status_print_unmerged_header
 DECL|function|wt_status_print_updated
 DECL|function|wt_status_print_verbose
 DECL|function|wt_status_truncate_message_at_cut_line
+DECL|macro|LABEL
 DECL|macro|quote_path
 DECL|member|buf
 DECL|member|nsha1
