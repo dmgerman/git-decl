961e7e9bd15ffba82283aa547df5407edc02842d
Kirill Smelkov <kirr@mns.spb.ru>
2014-03-27 18:24:38 +0400

    tree-diff: rework diff_tree interface to be sha1 based
    In the next commit this will allow to reduce intermediate calls, when
    recursing into subtrees - at that stage we know only subtree sha1, and
    it is natural for tree walker to start from that phase. For now we do
    
        diff_tree
            show_path
                diff_tree_sha1
                    diff_tree
                        ...
    
    and the change will allow to reduce it to
    
        diff_tree
            show_path
                diff_tree
    
    Also, it will allow to omit allocating strbuf for each subtree, and just
    reuse the common strbuf via playing with its len.
    
    The above-mentioned improvements go in the next 2 patches.
    
    The downside is that try_to_follow_renames(), if active, we cause
    re-reading of 2 initial trees, which was negligible based on my timings,
    and which is outweighed cogently by the upsides.
    
    NOTE To keep with the current interface and semantics, I needed to
    rename the function from diff_tree() to diff_tree_sha1(). As
    diff_tree_sha1() was already used, and the function we are talking here
    is its more low-level helper, let's use convention for prefixing
    such helpers with "ll_". So the final renaming is
    
        diff_tree() -> ll_diff_tree_sha1()
    
    Signed-off-by: Kirill Smelkov <kirr@mns.spb.ru>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- tree-diff.c
+++ tree-diff.c
@@ -1,8 +1,8 @@
 DECL|function|diff_might_be_rename
 DECL|function|diff_root_tree_sha1
-DECL|function|diff_tree
 DECL|function|diff_tree_sha1
 DECL|function|emit_diff
+DECL|function|ll_diff_tree_sha1
 DECL|function|show_path
 DECL|function|skip_uninteresting
 DECL|function|tree_entry_pathcmp
