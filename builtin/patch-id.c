67425d30d46947e66963709c1c084ae35d3982c6
Michael S. Tsirkin <mst@redhat.com>
2014-04-27 21:15:44 +0300

    patch-id: make it stable against hunk reordering
    Patch id changes if users reorder file diffs that make up a patch.
    
    As the result is functionally equivalent, a different patch id is
    surprising to many users.
    In particular, reordering files using diff -O is helpful to make patches
    more readable (e.g. API header diff before implementation diff).
    
    Add an option to change patch-id behaviour making it stable against
    these kinds of patch change:
    calculate SHA1 hash for each hunk separately and sum all hashes
    (using a symmetrical sum) to get patch id
    
    We use a 20byte sum and not xor - since xor would give 0 output
    for patches that have two identical diffs, which isn't all that
    unlikely (e.g. append the same line in two places).
    
    The new behaviour is enabled
    - when patchid.stable is true
    - when --stable flag is present
    
    Using a new flag --unstable or setting patchid.stable to false force
    the historical behaviour.
    
    In the documentation, clarify that patch ID can now be a sum of hashes,
    not a hash.
    Document how command line and config options affect the
    behaviour.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/patch-id.c
+++ builtin/patch-id.c
@@ -1,7 +1,9 @@
 DECL|function|cmd_patch_id
 DECL|function|flush_current_id
+DECL|function|flush_one_hunk
 DECL|function|generate_id_list
 DECL|function|get_one_patchid
+DECL|function|git_patch_id_config
 DECL|function|remove_space
 DECL|function|scan_hunk_header
 DECL|variable|patch_id_usage
