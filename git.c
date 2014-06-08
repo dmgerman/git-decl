80c9ceea3740c0c329095a7e60f1022fbd2774ef
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-06-08 16:37:10 +0700

    git potty: restore environments after alias expansion
    Commit 4ad8332 (t0001: test git init when run via an alias -
    2010-11-26) noted breakages when running init via alias. The problem
    is for alias to be used, $GIT_DIR must be searched, but 'init' and
    'clone' are not happy with that. So we start a new process like an
    external command, with clean environment in this case. Env variables
    that are set by command line (e.g. "git --git-dir=.. ") are kept.
    
    This should also fix autocorrecting a command typo to "init" because
    it's the same problem: aliases are read, then "init" is unhappy with
    $GIT_DIR already set up because of that.
    
    Reminded-by: David Turner <dturner@twopensource.com>
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git.c
+++ git.c
@@ -5,9 +5,12 @@ DECL|function|handle_builtin
 DECL|function|handle_options
 DECL|function|is_builtin
 DECL|function|main
+DECL|function|restore_env
 DECL|function|run_argv
 DECL|function|run_builtin
+DECL|function|save_env
 DECL|macro|NEED_WORK_TREE
+DECL|macro|NO_SETUP
 DECL|macro|RUN_SETUP
 DECL|macro|RUN_SETUP_GENTLY
 DECL|macro|USE_PAGER
@@ -16,7 +19,11 @@ DECL|member|fn
 DECL|member|option
 DECL|struct|cmd_struct
 DECL|variable|commands
+DECL|variable|env_names
 DECL|variable|git_more_info_string
 DECL|variable|git_startup_info
 DECL|variable|git_usage_string
+DECL|variable|orig_cwd
+DECL|variable|orig_env
+DECL|variable|saved_environment
 DECL|variable|use_pager
