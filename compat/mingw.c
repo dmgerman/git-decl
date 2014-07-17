49d1216e4599645c6ac03aef443dabd55fd03d48
Karsten Blees <blees@dcon.de>
2014-07-17 17:37:54 +0200

    Revert "Windows: teach getenv to do a case-sensitive search"
    This reverts commit df599e9612788b728ce43a03159b85f1fe624d6a.
    
    As of 5e9637c6 "i18n: add infrastructure for translating Git with gettext",
    eval_gettext uses MinGW envsubst.exe instead of git-sh-i18n--envsubst.exe
    for variable substitution. This breaks git-submodule.sh messages and tests,
    as envsubst.exe doesn't support case-sensitive environment lookup (the same
    is true for almost everything on Windows, including MSys and Cygwin tools).
    
    30a615ac "Windows/i18n: rename $path to prevent clashes with $PATH" renames
    the conflicting variable in git-submodule.sh, so that it works on Windows
    (i.e. with case-insensitive environment, regardless of the toolset).
    
    Revert to the documented behaviour of case-insensitive environment on
    Windows.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Stepan Kasal <kasal@ucw.cz>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -15,7 +15,6 @@ DECL|function|free_path_split
 DECL|function|freeaddrinfo_stub
 DECL|function|get_path_split
 DECL|function|getaddrinfo_stub
-DECL|function|getenv_cs
 DECL|function|getnameinfo_stub
 DECL|function|getpwuid
 DECL|function|gettimeofday
