ba6d56e62ef9d158e58a73b15b6b11db809c77c7
Christian Couder <chriscool@tuxfamily.org>
2014-07-19 17:01:12 +0200

    replace: remove signature when using --graft
    It could be misleading to keep a signature in a
    replacement commit, so let's remove it.
    
    Note that there should probably be a way to sign
    the replacement commit created when using --graft,
    but this can be dealt with in another commit or
    patch series.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- commit.c
+++ commit.c
@@ -68,6 +68,7 @@ DECL|function|record_author_date
 DECL|function|reduce_heads
 DECL|function|register_commit_graft
 DECL|function|remove_redundant
+DECL|function|remove_signature
 DECL|function|set_commit_buffer
 DECL|function|sort_in_topological_order
 DECL|function|standard_header_field
