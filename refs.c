7ec6391abf42e523149cedcd4d6a281328fb338d
Ronnie Sahlberg <sahlberg@google.com>
2014-09-03 11:45:43 -0700

    refs.c: allow listing and deleting badly named refs
    We currently do not handle badly named refs well:
    
      $ cp .git/refs/heads/master .git/refs/heads/master.....@\*@\.
      $ git branch
        fatal: Reference has invalid format: 'refs/heads/master.....@*@\.'
      $ git branch -D master.....@\*@\.
        error: branch 'master.....@*@\.' not found.
    
    Users cannot recover from a badly named ref without manually finding
    and deleting the loose ref file or appropriate line in packed-refs.
    Making that easier will make it easier to tweak the ref naming rules
    in the future, for example to forbid shell metacharacters like '<BACKTICK>'
    and '"', without putting people in a state that is hard to get out of.
    
    So allow "branch --list" to show these refs and allow "branch -d/-D"
    and "update-ref -d" to delete them.  Other commands (for example to
    rename refs) will continue to not handle these refs but can be changed
    in later patches.
    
    Details:
    
    In resolving functions, refuse to resolve refs that don't pass the
    git-check-ref-format(1) check unless the new RESOLVE_REF_ALLOW_BAD_NAME
    flag is passed.  Even with RESOLVE_REF_ALLOW_BAD_NAME, refuse to
    resolve refs that escape the refs/ directory and do not match the
    pattern [A-Z_]* (think "HEAD" and "MERGE_HEAD").
    
    In locking functions, refuse to act on badly named refs unless they
    are being deleted and either are in the refs/ directory or match [A-Z_]*.
    
    Just like other invalid refs, flag resolved, badly named refs with the
    REF_ISBROKEN flag, treat them as resolving to null_sha1, and skip them
    in all iteration functions except for for_each_rawref.
    
    Flag badly named refs (but not symrefs pointing to badly named refs)
    with a REF_BAD_NAME flag to make it easier for future callers to
    notice and handle them specially.  For example, in a later patch
    for-each-ref will use this flag to detect refs whose names can confuse
    callers parsing for-each-ref output.
    
    In the transaction API, refuse to create or update badly named refs,
    but allow deleting them (unless they try to escape refs/ and don't match
    [A-Z_]*).
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -69,7 +69,6 @@ DECL|function|get_packed_ref_dir
 DECL|function|get_packed_refs
 DECL|function|get_ref_cache
 DECL|function|get_ref_dir
-DECL|function|handle_missing_loose_ref
 DECL|function|head_ref
 DECL|function|head_ref_namespaced
 DECL|function|head_ref_submodule
@@ -114,6 +113,7 @@ DECL|function|ref_transaction_update
 DECL|function|ref_update_compare
 DECL|function|ref_update_reject_duplicates
 DECL|function|reflog_exists
+DECL|function|refname_is_safe
 DECL|function|refname_match
 DECL|function|release_packed_ref_cache
 DECL|function|remove_empty_directories
@@ -126,6 +126,7 @@ DECL|function|report_refname_conflict
 DECL|function|resolve_gitlink_packed_ref
 DECL|function|resolve_gitlink_ref
 DECL|function|resolve_gitlink_ref_recursive
+DECL|function|resolve_missing_loose_ref
 DECL|function|resolve_ref_unsafe
 DECL|function|resolve_refdup
 DECL|function|rollback_packed_refs
