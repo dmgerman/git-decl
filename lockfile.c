d0ed8d2db5fa58b149d1597611912d432023953f
Michael Haggerty <mhagger@alum.mit.edu>
2014-10-01 12:28:38 +0200

    lockfile.c: rename static functions
    * remove_lock_file() -> remove_lock_files()
    * remove_lock_file_on_signal() -> remove_lock_files_on_signal()
    
    Suggested-by: Torsten Bögershausen <tboegi@web.de>
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- lockfile.c
+++ lockfile.c
@@ -5,8 +5,8 @@ DECL|function|hold_lock_file_for_append
 DECL|function|hold_lock_file_for_update
 DECL|function|hold_locked_index
 DECL|function|lock_file
-DECL|function|remove_lock_file
-DECL|function|remove_lock_file_on_signal
+DECL|function|remove_lock_files
+DECL|function|remove_lock_files_on_signal
 DECL|function|reopen_lock_file
 DECL|function|resolve_symlink
 DECL|function|rollback_lock_file
