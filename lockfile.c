59bb9f483c1f5587a80ee03e524b947bd56e9c73
Michael Haggerty <mhagger@alum.mit.edu>
2014-10-01 12:28:05 +0200

    unable_to_lock_die(): rename function from unable_to_lock_index_die()
    This function is used for other things besides the index, so rename it
    accordingly.
    
    Suggested-by: Jeff King <peff@peff.net>
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Reviewed-by: Ronnie Sahlberg <sahlberg@google.com>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- lockfile.c
+++ lockfile.c
@@ -10,8 +10,8 @@ DECL|function|remove_lock_file_on_signal
 DECL|function|reopen_lock_file
 DECL|function|resolve_symlink
 DECL|function|rollback_lock_file
+DECL|function|unable_to_lock_die
 DECL|function|unable_to_lock_error
-DECL|function|unable_to_lock_index_die
 DECL|function|unable_to_lock_message
 DECL|macro|MAXDEPTH
 DECL|variable|lock_file_list
