1023fcdcf68f5d37559ac04bafedfae73abc9590
Junio C Hamano <gitster@pobox.com>
2014-10-14 14:42:04 -0700

    checkout: report upstream correctly even with loosely defined branch.*.merge
    When checking out a branch that is set to build on top of another
    branch (often, a remote-tracking branch), "git checkout" reports how
    your work relates to the other branch, e.g.
    
        Your branch is behind 'origin/master', and can be fast-forwarded.
    
    Back when this feature was introduced, this was only done for
    branches that build on remote-tracking branches, but 5e6e2b48 (Make
    local branches behave like remote branches when --tracked,
    2009-04-01) added support to give the same report for branches that
    build on other local branches (i.e. branches whose branch.*.remote
    variables are set to '.').  Unlike the support for the branches
    building on remote-tracking branches, however, this did not take
    into account the fact that branch.*.merge configuration is allowed
    to record a shortened branch name.
    
    When branch.*.merge is set to 'master' (not 'refs/heads/master'),
    i.e. "my branch builds on the local 'master' branch", this caused
    "git checkout" to report:
    
        Your branch is based on 'master', but the upstream is gone.
    
    The upstream is our repository and is definitely not gone, so this
    output is nonsense.
    
    The fix is fairly obvious; just like the branch name is DWIMed when
    "git pull" merges from the 'master' branch without complaint on such
    a branch, the name of the branch the current branch builds upon
    needs to be DWIMed the same way.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -84,6 +84,7 @@ DECL|function|remote_has_url
 DECL|function|remote_is_configured
 DECL|function|remote_tracking
 DECL|function|resolve_remote_symref
+DECL|function|set_merge
 DECL|function|set_ref_status_for_push
 DECL|function|sort_ref_list
 DECL|function|stat_tracking_info
