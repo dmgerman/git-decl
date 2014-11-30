cc70394f615baa548c2400841c952a5d8a8e34eb
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-11-30 15:24:41 +0700

    setup.c: detect $GIT_COMMON_DIR in is_git_directory()
    If the file "$GIT_DIR/commondir" exists, it contains the value of
    $GIT_COMMON_DIR.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -6,6 +6,7 @@ DECL|function|check_repository_format_gently
 DECL|function|check_repository_format_version
 DECL|function|daemonize
 DECL|function|die_verify_filename
+DECL|function|get_common_dir
 DECL|function|get_device_or_die
 DECL|function|git_config_perm
 DECL|function|is_git_directory
