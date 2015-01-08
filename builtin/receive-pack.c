5c08d8d6a6b979d9ae58a5fef43aa891804ad71b
Stefan Beller <sbeller@google.com>
2015-01-07 19:23:19 -0800

    receive-pack.c: add execute_commands_atomic function
    This introduces the new function execute_commands_atomic which will use
    one atomic transaction for all updates. The default behavior is still
    the old non atomic way, one ref at a time. This is to cause as little
    disruption as possible to existing clients. It is unknown if there are
    client scripts that depend on the old non-atomic behavior so we make it
    opt-in for now.
    
    A later patch will add the possibility to actually use the functionality
    added by this patch. For now use_atomic is always 0.
    
    Inspired-by: Ronnie Sahlberg <sahlberg@google.com>
    Helped-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -12,6 +12,7 @@ DECL|function|command_singleton_iterator
 DECL|function|copy_to_sideband
 DECL|function|delete_only
 DECL|function|execute_commands
+DECL|function|execute_commands_atomic
 DECL|function|execute_commands_non_atomic
 DECL|function|feed_receive_hook
 DECL|function|find_header
@@ -108,4 +109,5 @@ DECL|variable|transaction
 DECL|variable|transfer_fsck_objects
 DECL|variable|transfer_unpack_limit
 DECL|variable|unpack_limit
+DECL|variable|use_atomic
 DECL|variable|use_sideband
