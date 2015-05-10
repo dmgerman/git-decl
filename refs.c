02cfc27c0579bcddac9cbd8c2d7006b08a7af963
Michael Haggerty <mhagger@alum.mit.edu>
2015-05-10 04:45:33 +0200

    commit_ref_update(): new function, extracted from write_ref_sha1()
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -20,6 +20,7 @@ DECL|function|clear_ref_dir
 DECL|function|close_ref
 DECL|function|commit_packed_refs
 DECL|function|commit_ref
+DECL|function|commit_ref_update
 DECL|function|copy_msg
 DECL|function|create_dir_entry
 DECL|function|create_ref_cache
