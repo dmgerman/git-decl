b1909d0c9106f19c7f7b74a8d187257dfe587bc3
David Turner <dturner@twopensource.com>
2015-05-20 13:03:39 -0400

    sha1_name: get_sha1_with_context learns to follow symlinks
    Wire up get_sha1_with_context to call get_tree_entry_follow_symlinks
    when GET_SHA1_FOLLOW_SYMLINKS is passed in flags. G_S_FOLLOW_SYMLINKS
    is incompatible with G_S_ONLY_TO_DIE because the diagnosis
    that ONLY_TO_DIE triggers does not at present consider symlinks, and
    it would be a significant amount of additional code to allow it to
    do so.
    
    Signed-off-by: David Turner <dturner@twopensource.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -167,6 +167,7 @@ DECL|macro|FOR_EACH_OBJECT_LOCAL_ONLY
 DECL|macro|GET_SHA1_BLOB
 DECL|macro|GET_SHA1_COMMIT
 DECL|macro|GET_SHA1_COMMITTISH
+DECL|macro|GET_SHA1_FOLLOW_SYMLINKS
 DECL|macro|GET_SHA1_ONLY_TO_DIE
 DECL|macro|GET_SHA1_QUIETLY
 DECL|macro|GET_SHA1_TREE
@@ -390,6 +391,7 @@ DECL|member|sha1
 DECL|member|sha1
 DECL|member|sizep
 DECL|member|split_index
+DECL|member|symlink_path
 DECL|member|timestamp
 DECL|member|total_in
 DECL|member|total_out
