32687cc8e56d505aced0e41b17e7f39e9000e286
Paul Tan <pyokagan@gmail.com>
2015-06-14 16:41:53 +0800

    pull: pass verbosity, --progress flags to fetch and merge
    7f87aff (Teach/Fix pull/fetch -q/-v options, 2008-11-15) taught git-pull
    to accept the verbosity -v and -q options and pass them to git-fetch and
    git-merge.
    
    Re-implement support for the verbosity flags by adding it to the options
    list and introducing argv_push_verbosity() to push the flags into the
    argv array used to execute git-fetch and git-merge.
    
    9839018 (fetch and pull: learn --progress, 2010-02-24) and bebd2fd
    (pull: propagate --progress to merge, 2011-02-20) taught git-pull to
    accept the --progress option and pass it to git-fetch and git-merge.
    
    Use OPT_PASSTHRU() implemented earlier to pass the "--[no-]progress"
    command line options to git-fetch and git-merge.
    
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Paul Tan <pyokagan@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/pull.c
+++ builtin/pull.c
@@ -1,6 +1,9 @@
+DECL|function|argv_push_verbosity
 DECL|function|cmd_pull
 DECL|function|parse_repo_refspecs
 DECL|function|run_fetch
 DECL|function|run_merge
+DECL|variable|opt_progress
+DECL|variable|opt_verbosity
 DECL|variable|pull_options
 DECL|variable|pull_usage
