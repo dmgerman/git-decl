b1a6ee98d0e1ec6d8e3a8f8415c5bf63c21c78c0
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-06-22 17:25:09 +0200

    fsck: introduce identifiers for fsck messages
    Instead of specifying whether a message by the fsck machinery constitutes
    an error or a warning, let's specify an identifier relating to the
    concrete problem that was encountered. This is necessary for upcoming
    support to be able to demote certain errors to warnings.
    
    In the process, simplify the requirements on the calling code: instead of
    having to handle full-blown varargs in every callback, we now send a
    string buffer ready to be used by the callback.
    
    We could use a simple enum for the message IDs here, but we want to
    guarantee that the enum values are associated with the appropriate
    message types (i.e. error or warning?). Besides, we want to introduce a
    parser in the next commit that maps the string representation to the
    enum value, hence we use the slightly ugly preprocessor construct that
    is extensible for use with said parser.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fsck.c
+++ fsck.c
@@ -1,7 +1,10 @@
+DECL|enumerator|FOREACH_MSG_ID
+DECL|enum|fsck_msg_id
 DECL|function|fsck_commit
 DECL|function|fsck_commit_buffer
 DECL|function|fsck_error_function
 DECL|function|fsck_ident
+DECL|function|fsck_msg_type
 DECL|function|fsck_object
 DECL|function|fsck_tag
 DECL|function|fsck_tag_buffer
@@ -10,7 +13,15 @@ DECL|function|fsck_walk
 DECL|function|fsck_walk_commit
 DECL|function|fsck_walk_tag
 DECL|function|fsck_walk_tree
+DECL|function|report
 DECL|function|require_end_of_header
 DECL|function|verify_ordered
+DECL|macro|FOREACH_MSG_ID
+DECL|macro|MSG_ID
+DECL|macro|MSG_ID
+DECL|macro|MSG_ID
+DECL|macro|MSG_ID
 DECL|macro|TREE_HAS_DUPS
 DECL|macro|TREE_UNORDERED
+DECL|member|msg_type
+DECL|variable|msg_id_info
