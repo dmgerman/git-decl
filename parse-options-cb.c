a0889846fbecfc892cdc2e091a11f2bb6daf7e3e
Karthik Nayak <karthik.188@gmail.com>
2015-07-07 21:36:08 +0530

    tag: libify parse_opt_points_at()
    Rename 'parse_opt_points_at()' to 'parse_opt_object_name()' and
    move it from 'tag.c' to 'parse-options'. This now acts as a common
    parse_opt function which accepts an objectname and stores it into
    a sha1_array.
    
    Based-on-patch-by: Jeff King <peff@peff.net>
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: Matthieu Moy <matthieu.moy@grenoble-inp.fr>
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options-cb.c
+++ parse-options-cb.c
@@ -3,6 +3,7 @@ DECL|function|parse_opt_approxidate_cb
 DECL|function|parse_opt_color_flag_cb
 DECL|function|parse_opt_expiry_date_cb
 DECL|function|parse_opt_noop_cb
+DECL|function|parse_opt_object_name
 DECL|function|parse_opt_string_list
 DECL|function|parse_opt_tertiary
 DECL|function|parse_opt_verbosity_cb
