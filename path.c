505d6afa637980ced495d75f3b3124c1721daf62
Jeff King <peff@peff.net>
2015-08-10 05:32:22 -0400

    cache.h: complete set of git_path_submodule helpers
    The git_path function has "git_pathdup" and
    "strbuf_git_path" variants, but git_submodule_path only
    comes in the dangerous, static-buffer variant. That makes
    refactoring callers to use the safer functions hard (since
    they don't exist).
    
    Since we're already using a strbuf behind the scenes, it's
    easy to expose all three of these interfaces with thin
    wrappers.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- path.c
+++ path.c
@@ -6,6 +6,7 @@ DECL|function|cleanup_path
 DECL|function|daemon_avoid_alias
 DECL|function|dir_prefix
 DECL|function|do_git_path
+DECL|function|do_submodule_path
 DECL|function|enter_repo
 DECL|function|expand_user_path
 DECL|function|get_pathname
@@ -14,6 +15,7 @@ DECL|function|getpw_str
 DECL|function|git_path
 DECL|function|git_path_submodule
 DECL|function|git_pathdup
+DECL|function|git_pathdup_submodule
 DECL|function|have_same_root
 DECL|function|is_dir_file
 DECL|function|is_ntfs_dotgit
@@ -30,6 +32,7 @@ DECL|function|replace_dir
 DECL|function|report_linked_checkout_garbage
 DECL|function|strbuf_cleanup_path
 DECL|function|strbuf_git_path
+DECL|function|strbuf_git_path_submodule
 DECL|function|strip_path_suffix
 DECL|function|update_common_dir
 DECL|function|validate_headref
